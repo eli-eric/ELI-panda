0139926a423fcb706f20acf37f4771b98a0c679e
Jiri Svacha <jiri.svacha@eli-beams.eu>
2022-07-15 15:46:25 +0200

    import excle categories tool

--- tools/catalogue-excel-import/main.go
+++ tools/catalogue-excel-import/main.go
@@ -8,6 +8,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/xuri/excelize/v2"
 )
@@ -28,6 +29,25 @@ type CatalogueCategory struct {
 	ID_parent *int32
 	Name      string
 	Code      string
+	Groups    []CatalogueCatgeoryGroup
+}
+
+type CatalogueCatgeoryGroup struct {
+	ID         int32
+	Name       string
+	Properties []CatalogueCategoryProperty
+}
+
+type CatalogueCategoryProperty struct {
+	ID       int32
+	Name     string
+	ID_group int32
+}
+
+type CatalogCategoryPropertyType struct {
+	ID    int32
+	Name  string
+	IsLOV bool
 }
 
 var pgPool *pgxpool.Pool
@@ -87,11 +107,11 @@ func main() {
 		//so we have regular excel file and now we can process this file
 		//we will start by extracting sheet names
 		sheetNames := excelFile.GetSheetList()
+
 		// we expect two types of sheets:
 		// 1. Catalogue category definition sheet - this type of sheet has to have "Property name" on position A1 and "Category" on the position B1
 		// 2. Catalogue items data sheet - this type of sheet has to have "Catalogue items" in the name - could be wherever you want
 		// We are processing the sheets in the index order in the Workbook so it is recommended to have the sheets with a definition of categories first and then the catalogue items sheets
-
 		for _, name := range sheetNames {
 
 			//lets detect the type of sheet and depending on the type do the job
@@ -141,6 +161,24 @@ func getAndCacheAllCategories() error {
 	return nil
 }
 
+//this funtion check if the category exists by passing name as a parameter
+func existCategoryByName(name string) (bool, int32) {
+	var resultExists bool
+	var resultID int32
+
+	for i := range catalogueCategories {
+
+		if strings.Contains(strings.ToLower(catalogueCategories[i].Name), strings.ToLower(name)) {
+			resultExists = true
+			resultID = catalogueCategories[i].ID
+
+			break
+		}
+	}
+
+	return resultExists, resultID
+}
+
 func processCatalogueCategorySheet(sheetName string) {
 
 	//job start log
@@ -152,19 +190,37 @@ func processCatalogueCategorySheet(sheetName string) {
 
 	//get category and parent cateogry names
 	categoryName, _ := excelFile.GetCellValue(sheetName, "B4")
-	//parentCategoryName, _ := excelFile.GetCellValue(sheetName, "C4")
+	parentCategoryName, _ := excelFile.GetCellValue(sheetName, "C4")
 
 	//lets find if this category exist - if so do nothing
-	var categoryExist int
-	pgPool.QueryRow(context.Background(), `SELECT count(*) FROM panda.t_catalog_category tcc WHERE tcc."name" ILIKE $1`, categoryName).Scan(&categoryExist)
-
-	if categoryExist > 0 {
-		fmt.Println(categoryName + " EXISTS")
-	} else {
-		fmt.Println(categoryName + " NOT EXISTS")
-		createCatalogueCategory(categoryName)
+	var categoryExists, categoryID = existCategoryByName(categoryName)
+	//if category doesnt exist -> create into the DB -> get back new id and store in memory
+	if !categoryExists {
+		id, err := createCatalogueCategory(categoryName)
+		categoryID = id
+		if err != nil {
+			fmt.Println("ERROR: ", err)
+		}
 	}
+	//now lets have a look on parent category
+	//check if already exists such a category
+	//if parent name is empty do nothing
+	if parentCategoryName != "" {
 
+		var parentExists, parentID = existCategoryByName(parentCategoryName)
+
+		//if doesnt create this category first
+		if !parentExists {
+			id, err := createCatalogueCategory(parentCategoryName)
+			if err != nil {
+				fmt.Println("ERROR: ", err)
+			}
+			parentID = id
+		}
+		//finally set parent id to the category - we are doing it everytime for sure
+		setCategoryParentID(categoryID, parentID)
+
+	}
 	//job end log
 	end := time.Now()
 	fmt.Printf("Finished at: %s", end.String())
@@ -175,13 +231,57 @@ func processCatalogueCategorySheet(sheetName string) {
 	categorySheetsCount++
 }
 
-func createCatalogueCategory(categoryName string) error {
-	insertQuery := `INSERT INTO panda.t_catalog_category (id_parent, "name", code) VALUES (NULL,$1, $1)`
-	_, err := pgPool.Exec(context.Background(), insertQuery, categoryName)
+func setCategoryParentID(categoryID, parentID int32) error {
+	//first set it into the DB
+	updateQuery := `UPDATE panda.t_catalog_category SET id_parent = $2 WHERE id = $1`
+	_, err := pgPool.Exec(context.Background(), updateQuery, categoryID, parentID)
 	if err != nil {
 		return err
 	}
+	//then to the memory
+	for i := range catalogueCategories {
+
+		if catalogueCategories[i].ID == categoryID {
+			catalogueCategories[i].ID_parent = &parentID
+			break
+		}
+	}
+
 	return nil
+}
+
+func createCatalogueCategory(categoryName string) (int32, error) {
+	var newID int32
+	//one thing is to add category into the database
+	//in the second step we have to add category to local cache
+	tx, txErr := pgPool.BeginTx(context.Background(), pgx.TxOptions{})
+	if txErr == nil {
+		insertQuery := `INSERT INTO panda.t_catalog_category (id_parent, "name", code) VALUES (NULL,$1, $1)`
+		_, err := tx.Exec(context.Background(), insertQuery, categoryName)
+		if err != nil {
+			return newID, err
+		}
+		//we are interested in a new record id
+		newIdQuery := `SELECT LASTVAL()`
+		newIdRowErr := tx.QueryRow(context.Background(), newIdQuery).Scan(&newID)
+		if newIdRowErr != nil {
+			return newID, newIdRowErr
+		}
+		commitErr := tx.Commit(context.Background())
+		if commitErr != nil {
+			return newID, commitErr
+		}
+		//so after succesfull insert into db we can add the record to the memory
+		newCategory := CatalogueCategory{}
+		newCategory.Name = categoryName
+		newCategory.Code = categoryName
+		newCategory.ID = newID
+		catalogueCategories = append(catalogueCategories, newCategory)
+	} else {
+		return newID, txErr
+	}
+
+	return newID, nil
 }
 
 // detection of the sheet type
