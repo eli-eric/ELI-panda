6b7656f555b2590e6e2bf69b9bbe316f1deabd1a
Jiri Svacha <jiri.svacha@eli-beams.eu>
2022-07-18 12:52:27 +0200

    new css loader

--- frontend/src/@crema/core/AppLoader/index.tsx
+++ frontend/src/@crema/core/AppLoader/index.tsx
@@ -4,14 +4,14 @@ import "./loader.css";
 const AppLoader = () => {
   return (
     <div className="app-loader">
-      <div className="loader-spin">
-        <span className="crema-dot crema-dot-spin">
-          <i></i>
-          <i></i>
-          <i></i>
-          <i></i>
-          <i></i>
-        </span>
+      <div className="eli-logo">
+        <div className="eli-logo-p eli-logo-p1"></div>
+        <div className="eli-logo-p eli-logo-p2"></div>
+        <div className="eli-logo-p eli-logo-p3"></div>
+        <div className="eli-logo-p eli-logo-p4"></div>
+        <div className="eli-logo-p eli-logo-p5"></div>
+        <div className="eli-logo-p eli-logo-p6"></div>
+        <div className="eli-logo-p eli-logo-p7"></div>
       </div>
     </div>
   );
