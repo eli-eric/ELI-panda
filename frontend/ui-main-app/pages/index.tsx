4dedc80ea90536c2ceb0bb9fd03a2dbf94c63968
Jiří Švácha <jiri.svacha@eli-beams.eu>
2022-11-07 22:22:10 +0100

    #6 create new next.js app and move crema to old

---
+++ frontend/ui-main-app/pages/index.tsx
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+  return (
+    <div className="bg-orange-400 flex justify-center">
+      <div className="container bg-slate-100 h-screen">
+        <div className="flex justify-center">
+          <h1 className="text-3xl font-bold">
+            PANDA
+          </h1>
+        </div>
+      </div>
+    </div>
+  )
+}
