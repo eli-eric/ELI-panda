88cdd6fe01b450021f0bfbc10f672522b4950191
Jiří Švácha <jiri.svacha@eli-beams.eu>
2022-11-15 13:52:26 +0100

    #10 prepare test roles and users - mock

--- frontend/ui-main-app/pages/api/mock-server/authenticate.ts
+++ frontend/ui-main-app/pages/api/mock-server/authenticate.ts
@@ -9,14 +9,64 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<any>) 
       username: 'admin',
       email: 'albert.einstein@eli-laser.eu',
       facility: 'ELI ERIC',
-      roles: ['catalogue-view', 'systems-view'],
+      roles: ['basics', 'catalogue-view', 'systems-view', 'reports-view'],
       accessToken:
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJleHAiOiIxMjM0NTY3ODk5In0.4UuocEvJTR3c1t2kB4f5lbRKfs5yKe7dW1Qtm_LITII'
     }
 
-    res
-      .status(200)
-      .json(user)
+    res.status(200).json(user)
+  } else if (req.body && req.body.username === 'test1' && req.body.password === 'test1') {
+
+    const user = {
+      uid: '71864520-9e86-427c-901c-0c220f951776',
+      username: 'test1',
+      email: 'test1@eli-laser.eu',
+      facility: 'ELI ERIC',
+      roles: ['basics'],
+      accessToken:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJleHAiOiIxMjM0NTY3ODk5In0.4UuocEvJTR3c1t2kB4f5lbRKfs5yKe7dW1Qtm_LITII'
+    }
+
+    res.status(200).json(user)
+  } else if (req.body && req.body.username === 'test2' && req.body.password === 'test2') {
+
+    const user = {
+      uid: '71864520-9e86-427c-901c-0c220f951777',
+      username: 'test2',
+      email: 'test2@eli-laser.eu',
+      facility: 'ELI ERIC',
+      roles: ['basics', 'catalogue-view'],
+      accessToken:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJleHAiOiIxMjM0NTY3ODk5In0.4UuocEvJTR3c1t2kB4f5lbRKfs5yKe7dW1Qtm_LITII'
+    }
+
+    res.status(200).json(user)
+  } else if (req.body && req.body.username === 'test3' && req.body.password === 'test3') {
+
+    const user = {
+      uid: '71864520-9e86-427c-901c-0c220f951776',
+      username: 'test3',
+      email: 'test3@eli-laser.eu',
+      facility: 'ELI ERIC',
+      roles: ['basics', 'catalogue-view', 'systems-view'],
+      accessToken:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJleHAiOiIxMjM0NTY3ODk5In0.4UuocEvJTR3c1t2kB4f5lbRKfs5yKe7dW1Qtm_LITII'
+    }
+
+    res.status(200).json(user)
+  } else if (req.body && req.body.username === 'test4' && req.body.password === 'test4') {
+
+    const user = {
+      uid: '71864520-9e86-427c-901c-0c220f951776',
+      username: 'test4',
+      email: 'test4@eli-laser.eu',
+      facility: 'ELI ERIC',
+      roles: ['basics', 'reports-view'],
+      accessToken:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJleHAiOiIxMjM0NTY3ODk5In0.4UuocEvJTR3c1t2kB4f5lbRKfs5yKe7dW1Qtm_LITII'
+    }
+
+    res.status(200).json(user)
   } else {
     res.status(401).json({ message: 'Unauthorized' })
   }
