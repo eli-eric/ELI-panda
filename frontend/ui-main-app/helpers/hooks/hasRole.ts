bb81793bcb493229a2154202e010d16c16c48837
Jan Smrčka <jan.smrck@gmail.com>
2022-11-16 09:39:16 +0100

    merge

--- frontend/ui-main-app/helpers/hooks/hasRole.ts
+++
@@ -1,6 +0,0 @@
-import { ROLES } from "types/constants/roles"
-
-export const hasRole = (userRoles: ROLES[] | undefined, role: ROLES) => {
-
-    return userRoles?.indexOf(role) !== -1
-}
--- frontend/ui-main-app/pages/catalogue/index.tsx
+++ frontend/ui-main-app/pages/catalogue/index.tsx
@@ -1,14 +1,9 @@
-import { hasRole } from 'helpers/hooks/hasRole'
 import { useAuth } from 'helpers/hooks/useAuth'
 import { NextPage } from 'next'
 import { Fragment } from 'react'
-import { ROLES } from 'types/constants/roles'
 
 const CataloguePage: NextPage = (): JSX.Element => {
-  const { status, userRoles } = useAuth()
-
-  const canViewCatalogue = hasRole(userRoles, ROLES.CATALOGUE_VIEW)
-  const canViewReports = hasRole(userRoles, ROLES.REPORTS_VIEW)
+  const { status } = useAuth()
 
   return (
     <Fragment>
@@ -21,24 +16,6 @@ const CataloguePage: NextPage = (): JSX.Element => {
             <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
               {/* Replace with your content */}
               <div className="py-4">
-                {canViewCatalogue && (
-                  <button
-                    type="button"
-                    className="inline-flex items-center rounded border border-transparent bg-indigo-600 px-2.5 py-1.5 mr-3 text-xs font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                  >
-                    VIEW
-                  </button>
-                )}
-                {canViewReports && (
-                  <button
-                    type="button"
-                    className="inline-flex items-center rounded border border-transparent bg-indigo-600 px-2.5 py-1.5 mr-3 text-xs font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                  >
-                    REPORTS
-                  </button>
-                )}
-
-
                 <div className="h-96 rounded-lg border-4 border-dashed border-gray-200" />
               </div>
               {/* /End replace */}
