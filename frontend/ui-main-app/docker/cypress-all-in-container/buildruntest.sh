26f76dc6aeb05abe880c4fdcbeefec65c4ae9410
Jiří Švácha <jiri.svacha@eli-beams.eu>
2022-11-25 18:32:40 +0100

    cypress integration

--- frontend/ui-main-app/docker/cypress-all-in-container/buildruntest.sh
+++ frontend/ui-main-app/docker/cypress-all-in-container/buildruntest.sh
@@ -4,9 +4,9 @@
 yarn start &
   
 # Start the second process
-yarn cypress run --browser chrome --tag "chrome" --env host=http://localhost:5001
-yarn cypress run --browser edge --tag "edge" --env host=http://localhost:5001
-yarn cypress run --browser firefox --tag "firefox" --env host=http://localhost:5001 &
+yarn cypress run --browser chrome --env host=http://localhost:5001
+yarn cypress run --browser edge --env host=http://localhost:5001
+yarn cypress run --browser firefox --env host=http://localhost:5001 &
   
 # Wait for any process to exit
 wait -n
