5ecaa3f9c979ac1bf5d00194849d8078b7566367
Jiří Švácha <jiri.svacha@eli-beams.eu>
2022-11-25 18:19:58 +0100

    integration of cypress

--- frontend/ui-main-app/cypress.config.ts
+++ frontend/ui-main-app/cypress.config.ts
@@ -4,9 +4,6 @@ export default defineConfig({
   projectId: "dnzwr8",
   taskTimeout: 10000,
   requestTimeout: 10000,
-  env: {
-    "host" : "http://172.17.0.1:5555"
-  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
---
+++ frontend/ui-main-app/docker/cypress-all-in-container/buildruntest.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Start the first process
+yarn start &
+  
+# Start the second process
+yarn cypress run --browser chrome --tag "chrome" --env host=http://localhost:5001
+yarn cypress run --browser edge --tag "edge" --env host=http://localhost:5001
+yarn cypress run --browser firefox --tag "firefox" --env host=http://localhost:5001 &
+  
+# Wait for any process to exit
+wait -n
+  
+# Exit with status of process that exited first
+exit $?
