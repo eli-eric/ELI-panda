86ca46135e9ae9fc8742d075bf1d6562d6456335
Jiří Švácha <jiri.svacha@eli-beams.eu>
2022-11-25 23:50:31 +0100

    test

--- frontend/ui-main-app/docker/cypress-all-in-container/buildruntest.sh
+++ frontend/ui-main-app/docker/cypress-all-in-container/buildruntest.sh
@@ -4,9 +4,13 @@
 yarn start &
   
 # Start the second process
-yarn cypress run --record --key $CYPRESS_RECORD_KEY --browser chrome --tag "chrome" --env host=http://localhost:5001
-yarn cypress run --record --key $CYPRESS_RECORD_KEY --browser edge --tag "edge" --env host=http://localhost:5001
-yarn cypress run --record --key $CYPRESS_RECORD_KEY --browser firefox --tag "firefox" --env host=http://localhost:5001 &
+# yarn cypress run --record --key $CYPRESS_RECORD_KEY --browser chrome --tag "chrome" --env host=http://localhost:5001
+# yarn cypress run --record --key $CYPRESS_RECORD_KEY --browser edge --tag "edge" --env host=http://localhost:5001
+# yarn cypress run --record --key $CYPRESS_RECORD_KEY --browser firefox --tag "firefox" --env host=http://localhost:5001 &
+
+yarn cypress run --browser chrome --tag "chrome" --env host=http://localhost:5001
+yarn cypress run --browser edge --tag "edge" --env host=http://localhost:5001
+yarn cypress run --browser firefox --tag "firefox" --env host=http://localhost:5001 &
   
 # Wait for any process to exit
 wait -n
