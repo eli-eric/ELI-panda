30f32ae648fcd1b133d56ffc270ee5f80491ddf0
Jiri Svacha <jiri.svacha@eli-beams.eu>
2022-11-28 10:06:32 +0100

    dynamic port

--- frontend/ui-main-app/docker/cypress-all-in-container/buildruntest.sh
+++ frontend/ui-main-app/docker/cypress-all-in-container/buildruntest.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
+port=5001
+
+
+
 # Start the first process
-yarn start &
+PORT=${port} yarn start &
   
 # Start the second process
 # yarn cypress run --record --key $CYPRESS_RECORD_KEY --browser chrome --tag "chrome" --env host=http://localhost:5001
 # yarn cypress run --record --key $CYPRESS_RECORD_KEY --browser edge --tag "edge" --env host=http://localhost:5001
 # yarn cypress run --record --key $CYPRESS_RECORD_KEY --browser firefox --tag "firefox" --env host=http://localhost:5001 &
 
-yarn cypress run --browser chrome --tag "chrome" --env host=http://localhost:5001
-yarn cypress run --browser edge --tag "edge" --env host=http://localhost:5001
-yarn cypress run --browser firefox --tag "firefox" --env host=http://localhost:5001 &
+yarn cypress run --browser chrome --tag "chrome" --env host=http://localhost:${port}
+yarn cypress run --browser edge --tag "edge" --env host=http://localhost:${port}
+yarn cypress run --browser firefox --tag "firefox" --env host=http://localhost:${port} &
   
 # Wait for any process to exit
 wait -n
