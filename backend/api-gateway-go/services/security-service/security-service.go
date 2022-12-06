2d779fb5b0de30bb337f229a550db4f6f9a0f25e
Jiří Švácha <jiri.svacha@eli-beams.eu>
2022-12-06 18:04:43 +0100

    improve neo4j db access #28

--- backend/api-gateway-go/services/security-service/security-service.go
+++ backend/api-gateway-go/services/security-service/security-service.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"panda/apigateway/config"
+	"panda/apigateway/helpers"
 	"panda/apigateway/ioutils"
 	"panda/apigateway/services/security-service/models"
 	"time"
@@ -54,35 +55,29 @@ func NewSecurityService(settings *config.Config) ISecurityService {
 func (svc *SecurityService) AuthenticateByUsernameAndPassword(username string, password string) (authUser models.UserAuthInfo, err error) {
 
 	// Open a new Session
-	session, _ := newNeo4jSession(svc.neo4jDriver)
+	session, _ := helpers.NewNeo4jSession(svc.neo4jDriver)
 
 	//the user has to be enabled
-	result, err := getNeo4jSingleRecord(session, `match(u:User{username: $userName})-[:HAS_ROLE]->(r:Role) 
-	return {uid: u.uid, 
+	authUser, err = helpers.GetNeo4jSingleRecordAndMapToStruct[models.UserAuthInfo](session, `match(u:User{username: $userName})-[:HAS_ROLE]->(r:Role) 
+	return {
 		passwordHash: u.passwordHash, 
 		lastName: u.lastName ,
 		firstName: u.firstName,
 		email: u.email, 
 		roles: collect(r.code)} as userInfo`, map[string]interface{}{"userName": username}, "userInfo")
 
+	fmt.Println(authUser)
 	//if there is a user in DB lets chekc the password
 	if err == nil {
-		// Check password
-		userProps := result.(map[string]interface{})
 
-		passwordHash := userProps["passwordHash"].(string)
-		// Throws unauthorized error
-		verifErr := bcrypt.CompareHashAndPassword([]byte(passwordHash), []byte(password))
-
+		verifErr := bcrypt.CompareHashAndPassword([]byte(authUser.PasswordHash), []byte(password))
+		//empty passwordHash -> omitempty json -> not sent to client
+		authUser.PasswordHash = ""
+		// Throws unauthorized error if there is verifErr
 		if verifErr == nil {
-			rolesI := userProps["roles"].([]interface{})
-			var roles = make([]string, 0)
-			if rolesI != nil && len(rolesI) > 0 {
-				// TODO
-			}
 			// Set custom claims
 			claims := &models.JwtCustomClaims{
-				Roles: roles,
+				Roles: authUser.Roles,
 				StandardClaims: jwt.StandardClaims{
 					ExpiresAt: time.Now().Add(time.Hour * 876000).Unix(),
 					Subject:   username,
@@ -95,13 +90,7 @@ func (svc *SecurityService) AuthenticateByUsernameAndPassword(username string, p
 			// Generate encoded token and send it as response.
 			token, err := newToken.SignedString([]byte(svc.jwtSecret))
 			if err == nil {
-				authUser = models.UserAuthInfo{
-					Uid:         userProps["uid"].(string),
-					Username:    username,
-					FullName:    userProps["firstName"].(string) + " " + userProps["lastName"].(string),
-					AccessToken: token,
-					Roles:       roles,
-				}
+				authUser.AccessToken = token
 			}
 
 			return authUser, err
@@ -125,58 +114,4 @@ func (svc *SecurityService) RefreshToken(claims *models.JwtCustomClaims) (string
 	}
 
 	return t, nil
-}
-
-func newNeo4jSession(driver neo4j.Driver) (neo4j.Session, error) {
-	session := driver.NewSession(neo4j.SessionConfig{})
-	var err error
-	defer func() {
-		err = ioutils.DeferredClose(session, err)
-	}()
-	return session, err
-}
-
-func getNeo4jSingleRecord(session neo4j.Session, cypher string, params map[string]interface{}, returnAlias string) (interface{}, error) {
-	result, err := session.ReadTransaction(func(tx neo4j.Transaction) (interface{}, error) {
-		result, err := tx.Run(cypher, params)
-		if err != nil {
-			return nil, err
-		}
-
-		record, err := result.Single()
-		if err != nil {
-			return nil, fmt.Errorf("record not found")
-		}
-
-		rec, _ := record.Get(returnAlias)
-		return rec, nil
-
-	})
-
-	return result, err
-}
-
-func neo4jReadArrayOfNodes(session neo4j.Session, cypher string, params map[string]interface{}, returnAlias string) (interface{}, error) {
-	// Execute a query in a new Read Transaction
-	results, err := session.ReadTransaction(func(tx neo4j.Transaction) (interface{}, error) {
-
-		result, err := tx.Run(cypher, params)
-		if err != nil {
-			return nil, err
-		}
-
-		// Get a list of Movies from the Result
-		records, err := result.Collect()
-		if err != nil {
-			return nil, err
-		}
-		var results []map[string]interface{}
-		for _, record := range records {
-			movie, _ := record.Get(returnAlias)
-			results = append(results, movie.(map[string]interface{}))
-		}
-		return results, nil
-	})
-
-	return results, err
 }
