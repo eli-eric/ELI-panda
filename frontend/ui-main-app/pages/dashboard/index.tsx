61cd21568d7b9e5c1953c1922ef97f1fc203e9c2
Jiri Svacha <jiri.svacha@eli-beams.eu>
2022-12-08 08:56:18 +0100

    improve relase history page

--- frontend/ui-main-app/pages/dashboard/index.tsx
+++ frontend/ui-main-app/pages/dashboard/index.tsx
@@ -5,13 +5,13 @@ import { Fragment } from 'react'
 import { useIntl } from 'react-intl'
 
 interface RelaseVersion {
-  id: string;
-  bugs?: RelaseNote[];
-  features?: RelaseNote[];
+  id: string
+  bugs?: RelaseNote[]
+  features?: RelaseNote[]
 }
 
 interface RelaseNote {
-  description: string;
+  description: string
 }
 
 const messages = message.dashboardPage
@@ -24,14 +24,19 @@ const DashboardPage: NextPage = (): JSX.Element => {
       id: '0.0.1',
       features: [
         {
-          description: "Catalogue page: searching items via single search bar"
+          description: 'Catalogue page: searching items via search bar - text box in the header of the page'
         },
         {
-          description: "Catalogue page: search by categories"
+          description: 'Catalogue page: filter by categories - categories tiles'
         },
         {
-          description: "Catalogue page: item detail"
-        },
+          description: 'Catalogue page: item detail page'
+        }
+      ],
+      bugs: [
+        {
+          description: 'Loading indicator - show loading info if there is a slow network connection'
+        }
       ]
     }
   ]
@@ -44,44 +49,40 @@ const DashboardPage: NextPage = (): JSX.Element => {
       </Head>
 
       <main className="flex-1">
-        <h1 className='text-2xl font-semibold font-mono text-gray-600 mt-2 ml-1 sm:mt-4 sm:ml-4 uppercase'>Release History</h1>
+        <h1 className="text-2xl font-semibold font-mono text-gray-600 mt-2 ml-1 sm:mt-4 sm:ml-4 uppercase">
+          Release History
+        </h1>
         <ul role="list" className="divide-y divide-gray-200 font-mono">
           {releases.map((item, idx) => (
             <li key={item.id} className="px-2 py-2 sm:px-4 sm:py-4 ">
-              <h3 className='text-xl font-bold text-gray-700'>Version {item.id} {idx === 0 && "- Latest version"}</h3>
-              <div className='p-2'>
+              <h3 className="text-xl font-bold text-gray-700">
+                Version {item.id} {idx === 0 && '- Latest version'}
+              </h3>
+              <div className="p-2">
                 {item.bugs && (
                   <>
-                    <div className='text-lg'>
-                      Bugs fixed:
-                    </div>
-                    <ul className='p-1'>
-                      {item.bugs.map((bug) => (
-                        <li key={bug.description}>
-                          - {bug.description}
-                        </li>
+                    <div className="text-lg text-red-500">Fixed bugs:</div>
+                    <ul className="p-1">
+                      {item.bugs.map(bug => (
+                        <li key={bug.description}>- {bug.description}</li>
                       ))}
                     </ul>
-                  </>)}
+                  </>
+                )}
                 {item.features && (
                   <>
-                    <div className='text-lg'>
-                      New features:
-                    </div>
-                    <ul className='p-1'>
-                      {item.features.map((feature) => (
-                        <li key={feature.description}>
-                          - {feature.description}
-                        </li>
+                    <div className="text-lg text-green-600">New features:</div>
+                    <ul className="p-1">
+                      {item.features.map(feature => (
+                        <li key={feature.description}>- {feature.description}</li>
                       ))}
                     </ul>
-                  </>)}
+                  </>
+                )}
               </div>
-
             </li>
           ))}
         </ul>
-
       </main>
     </Fragment>
   )
