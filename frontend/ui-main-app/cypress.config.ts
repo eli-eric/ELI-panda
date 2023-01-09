79e8b6631e2ef3a30802caea6483917604b7600c
Jiří Majer <juras.majer@gmail.com>
2023-01-09 18:25:39 +0100

    Update cypress.config.ts

--- frontend/ui-main-app/cypress.config.ts
+++ frontend/ui-main-app/cypress.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   projectId: "dnzwr8",
-  taskTimeout: 20000,
-  requestTimeout: 20000,
+  taskTimeout: 100000,
+  requestTimeout: 100000,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
