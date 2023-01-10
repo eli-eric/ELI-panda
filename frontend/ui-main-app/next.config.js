7e8ec574a854aac417f12ab326e124d60d13579b
Jiri Majer <juras.majer@gmail.com>
2023-01-10 21:08:04 +0100

    Test CI

--- frontend/ui-main-app/next.config.js
+++ frontend/ui-main-app/next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
   images: {
     domains: ['localhost', 'panda.eli-beams.eu']
   },
+
   env: {
     PANDA_API_GW_URL: process.env.PANDA_API_GW_URL,
     NODE_TLS_REJECT_UNAUTHORIZED: process.env.NODE_TLS_REJECT_UNAUTHORIZED
