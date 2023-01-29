4fc6c46e25114e2f1952b0a91ce99da602f3a359
Jan Smrčka <jan.smrck@gmail.com>
2023-01-29 18:21:30 +0100

    ErrorBoundary class component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,18 @@
 import '../styles/globals.css'
 
+import ErrorBoundary from 'components/error/ErrorBoundary'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
-import GlobalProvider from 'providers/global-provider'
+import GlobalProvider from 'providers/globalProvider'
 
 const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
   return (
     <SessionProvider session={session}>
-      <GlobalProvider>
-        <Component {...pageProps} />
-      </GlobalProvider>
+      <ErrorBoundary>
+        <GlobalProvider>
+          <Component {...pageProps} />
+        </GlobalProvider>
+      </ErrorBoundary>
     </SessionProvider>
   )
 }
--- providers/globalProvider.tsx
+++ providers/globalProvider.tsx
@@ -1,15 +1,17 @@
 import axios from 'axios'
-import AppLoader from 'components/loaders/app-loader.cont'
 import NavigationComponent from 'components/nav-bar/nav-bar.comp'
 import { messages } from 'i18n/src'
 import { useSession } from 'next-auth/react'
 import { IntlProvider } from 'react-intl'
-import { AppLoadingProvider } from 'store/app-loading.context'
-import { ComponentLoadingProvider } from 'store/component-loading.context'
 import { SWRConfig } from 'swr'
 
 interface Props {
   children: React.ReactNode
+}
+async function fetcher(url) {
+  const res = await axios.get(url).then(res => res.data)
+
+  return res
 }
 
 const GlobalProvider = ({ children }: Props) => {
@@ -20,18 +22,19 @@ const GlobalProvider = ({ children }: Props) => {
   return (
     <SWRConfig
       value={{
-        fetcher: url => axios.get(url).then(res => res.data)
+        fetcher: fetcher,
+        suspense: true,
+        onError: (error, key) => {
+          if (!error) {
+            const err = new Error('An error occurred while fetching the data.')
+            throw err
+          }
+        }
       }}
     >
       <IntlProvider locale="en" messages={messages['en']}>
-        <AppLoadingProvider>
-          <ComponentLoadingProvider>
-            <AppLoader>
-              <NavigationComponent />
-              {children}
-            </AppLoader>
-          </ComponentLoadingProvider>
-        </AppLoadingProvider>
+        <NavigationComponent />
+        {children}
       </IntlProvider>
     </SWRConfig>
   )
