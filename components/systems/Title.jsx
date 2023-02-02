9a933ca8bcf9714597fd8b769abe4a5b8e938fc3
Jiri Majer <juras.majer@gmail.com>
2023-02-02 21:29:23 +0100

    Add placeholder to Title input

--- components/systems/Title.jsx
+++ components/systems/Title.jsx
@@ -1,6 +1,10 @@
 const Title = ({ data, editMode }) => {
   const { isEditMode, register } = editMode
-  return isEditMode ? <input {...register('name')} className="w-full" /> : <h1 className="">{data.name}</h1>
+  return isEditMode ? (
+    <input placeholder="Title" {...register('name')} className="w-full" />
+  ) : (
+    <h1 className="">{data.name}</h1>
+  )
 }
 
 export default Title
