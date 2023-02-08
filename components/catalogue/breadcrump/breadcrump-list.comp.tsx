95db10a2ed871d0bdf8d723b086334b3aa1eb31a
Jan Smrčka <jan.smrck@gmail.com>
2023-02-08 11:55:01 +0100

    added chevronrigt icon

--- components/catalogue/breadcrump/breadcrump-list.comp.tsx
+++ components/catalogue/breadcrump/breadcrump-list.comp.tsx
@@ -1,4 +1,4 @@
-import { HomeIcon } from '@heroicons/react/20/solid'
+import { ChevronRightIcon, HomeIcon } from '@heroicons/react/20/solid'
 import { PlusIcon } from '@heroicons/react/24/outline'
 import ModalComponent from 'components/ui/modal/modal.comp'
 import { useRouter } from 'next/router'
@@ -41,16 +41,19 @@ const BreadcrumbListComponent = ({ navigationList, handleClick, testId }: Props)
           </li>
           {navigationList}
           <li className="flex">
-            <button
-              type="button"
-              onClick={() => {
-                setopen(true)
-              }}
-              className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-400 hover:bg-gray-50 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500"
-            >
-              <span className="sr-only">Delete</span>
-              <PlusIcon className="h-5 w-5" aria-hidden="true" />
-            </button>
+            <div className="flex items-center">
+              <ChevronRightIcon className="h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
+              <button
+                type="button"
+                onClick={() => {
+                  setopen(true)
+                }}
+                className="ml-3 relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-400 hover:bg-gray-50 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500"
+              >
+                <span className="sr-only">Delete</span>
+                <PlusIcon className="h-5 w-5" aria-hidden="true" />
+              </button>
+            </div>
           </li>
         </ol>
       </nav>
