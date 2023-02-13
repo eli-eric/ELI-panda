2e50997f6d81917486a6180158f358ccc5befe12
Jiri Majer <juras.majer@gmail.com>
2023-02-13 12:52:05 +0100

    Add tailwind plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -34,5 +34,10 @@ module.exports = {
       }
     }
   },
-  plugins: [require('@tailwindcss/forms')]
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio')
+  ]
 }
