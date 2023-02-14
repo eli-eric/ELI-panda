b0861826f73993ef8b8781e033c9751ddbeba9d8
Jan Smrčka <jan.smrck@gmail.com>
2023-02-14 14:08:07 +0100

    added general table component

--- src/components/systems/relations/RelationList.tsx
+++ src/components/systems/relations/RelationList.tsx
@@ -2,15 +2,16 @@ import { ArrowLongLeftIcon, ArrowLongRightIcon } from '@heroicons/react/24/outli
 
 import DisclosureComponent from '@/components/ui/Disclosure.comp'
 import { TrashIconButton } from '@/components/ui/IconButtons'
+import TableComponent from '@/components/ui/Table.comp'
 
-const people = [
-  { name: 'Blada bla', direction: 'from', relation: 'HAS_SUBSYSTEM' },
-  { name: 'Blad gbla', direction: 'to', relation: 'IS_SPARE_FOR' },
-  { name: 'Blad bla', direction: 'to', relation: 'HAS_SUBSYSTEM' },
-  { name: 'Bla fbla', direction: 'to', relation: 'IS_SPARE_FOR' },
-  { name: 'Bla gbla', direction: 'from', relation: 'IS_SPARE_FOR' }
+const relations = [
+  { direction: 'from', relation: 'HAS_SUBSYSTEM', name: 'Blada bla' },
+  { direction: 'to', relation: 'IS_SPARE_FOR', name: 'Blad gbla' },
+  { direction: 'to', relation: 'HAS_SUBSYSTEM', name: 'Blad bla' },
+  { direction: 'to', relation: 'IS_SPARE_FOR', name: 'Bla fbla' },
+  { direction: 'from', relation: 'IS_SPARE_FOR', name: 'Bla gbla' }
 
-  // More people...
+  // More relations...
 ]
 
 function classNames(...classes) {
@@ -18,6 +19,22 @@ function classNames(...classes) {
 }
 
 function Table() {
+  const collums = Object.keys(relations[0])
+  const data = relations.map((relation, index) => {
+    const rows = Object.entries(relation).map((value, index) => {
+      if (value[0] === 'direction') {
+        return (
+          <div key={index}>
+            {value[1] === 'to' && <ArrowLongLeftIcon className="w-10 h-10" />}
+            {value[1] === 'from' && <ArrowLongRightIcon className="w-10 h-10" />}
+          </div>
+        )
+      }
+      return <p key={index}>{value[1]}</p>
+    })
+    return [...rows, <TrashIconButton key={index + '1'} onClickAction={() => {}} rounded="rounded-md" />]
+  })
+
   return (
     <div className="px-4 sm:px-20 lg:px-20">
       <div className="sm:flex sm:items-center">
@@ -30,83 +47,9 @@ function Table() {
           </button>
         </div>
       </div>
-      <div className="mt-8 flex flex-col">
-        <div className="-my-2 -mx-4 sm:-mx-6 lg:-mx-8">
-          <div className="inline-block min-w-full py-2 align-middle">
-            <div className="shadow-sm ring-1 ring-black ring-opacity-5">
-              <table className="min-w-full border-separate" style={{ borderSpacing: 0 }}>
-                <thead className="bg-gray-50">
-                  <tr>
-                    <th
-                      scope="col"
-                      className="sticky top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8"
-                    >
-                      Direction
-                    </th>
-                    <th
-                      scope="col"
-                      className="sticky top-0 z-10 hidden border-b border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:table-cell"
-                    >
-                      Relation
-                    </th>
-                    <th
-                      scope="col"
-                      className="sticky top-0 z-10 hidden border-b border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter lg:table-cell"
-                    >
-                      System
-                    </th>
-                    <th
-                      scope="col"
-                      className="sticky top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 py-3.5 pr-4 pl-3 backdrop-blur backdrop-filter sm:pr-6 lg:pr-8"
-                    >
-                      <span className="sr-only">Edit</span>
-                    </th>
-                  </tr>
-                </thead>
-                <tbody className="bg-white">
-                  {people.map((person, personIdx) => (
-                    <tr key={person.name}>
-                      <td
-                        className={classNames(
-                          personIdx !== people.length - 1 ? 'border-b border-gray-200' : '',
-                          'whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8'
-                        )}
-                      >
-                        {person.direction === 'to' && <ArrowLongLeftIcon className="w-10 h-10" />}
-                        {person.direction === 'from' && <ArrowLongRightIcon className="w-10 h-10" />}
-                      </td>
-                      <td
-                        className={classNames(
-                          personIdx !== people.length - 1 ? 'border-b border-gray-200' : '',
-                          'whitespace-nowrap px-3 py-4 text-sm text-gray-500 hidden sm:table-cell'
-                        )}
-                      >
-                        {person.relation}
-                      </td>
-                      <td
-                        className={classNames(
-                          personIdx !== people.length - 1 ? 'border-b border-gray-200' : '',
-                          'whitespace-nowrap px-3 py-4 text-sm text-gray-500 hidden lg:table-cell'
-                        )}
-                      >
-                        {person.name}
-                      </td>
 
-                      <td
-                        className={classNames(
-                          personIdx !== people.length - 1 ? 'border-b border-gray-200' : '',
-                          'relative whitespace-nowrap py-4 pr-4 pl-3 text-right text-sm font-medium sm:pr-6 lg:pr-8'
-                        )}
-                      >
-                        <TrashIconButton onClickAction={() => {}} rounded="rounded-md" />
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
+      <div className="border rounded-md">
+        <TableComponent collumsTitle={[...collums, 'action']} tableRows={data} />
       </div>
     </div>
   )
---
+++ src/components/ui/Table.comp.tsx
@@ -0,0 +1,58 @@
+const ColumnTitle = ({ title }: { title: string }) => {
+  return (
+    <th
+      scope="col"
+      className="whitespace-nowrap sticky top-0 z-9 bg-gray-50 bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 "
+    >
+      {title}
+    </th>
+  )
+}
+
+const TableHeader = ({ collumsTitle }: { collumsTitle: string[] }) => {
+  return (
+    <thead className="bg-gray-50">
+      <tr>
+        {collumsTitle.map(title => (
+          <ColumnTitle key={title} title={title} />
+        ))}
+      </tr>
+    </thead>
+  )
+}
+
+const TableRow = ({ value }: { value: JSX.Element | string }) => (
+  <td className="whitespace-nowrap text-sm  sm:pl-6 text-gray-500">{value}</td>
+)
+
+interface Props {
+  collumsTitle: string[]
+  tableRows: JSX.Element[][]
+}
+
+const TableComponent = ({ collumsTitle, tableRows }: Props) => {
+  return (
+    <div className="h-full border-t border-gray-300  ">
+      <div className="-my-2  sm:-mx-6 lg:-mx-8">
+        <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+          <div className=" shadow ring-1 ring-black ring-opacity-5 ">
+            <table className="min-w-full divide-y divide-gray-300">
+              <TableHeader collumsTitle={collumsTitle} />
+              <tbody className="bg-white">
+                {tableRows.map((row, index) => (
+                  <tr key={index} className={(index % 2 === 0 ? undefined : 'bg-gray-100') + ' hover:bg-primary-200'}>
+                    {row.map(item => (
+                      <TableRow key={index} value={item} />
+                    ))}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default TableComponent
