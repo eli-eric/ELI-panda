a7854c738791631318d2a7c75af61bcbbea40c74
Jiri Majer <juras.majer@gmail.com>
2023-02-17 11:39:53 +0100

    Fix overflowing Progress Bar

--- src/components/ui/progress-bar.comp.tsx
+++ src/components/ui/progress-bar.comp.tsx
@@ -1,6 +1,6 @@
 const ProgressBarComponent = () => {
   return (
-    <div className="w-full h-[2px] bg-primary-100">
+    <div className="overflow-hidden w-full h-[2px] bg-primary-100">
       <div className="animate-progressBar w-full h-full origin-left bg-primary-300"></div>
     </div>
   )
