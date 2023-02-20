d5333404729016588adc2b78453de3f848e1ce47
Jiri Majer <juras.majer@gmail.com>
2023-02-20 12:21:20 +0100

    Fix system detail label's width

--- src/components/item-property/item-property-title.comp.tsx
+++ src/components/item-property/item-property-title.comp.tsx
@@ -8,7 +8,7 @@ interface Props {
 const ItemPropertyTitle = ({ title, children }: Props) => {
   return (
     <div className="flex gap-x-4 text-sm font-medium text-gray-400">
-      <dt className="">
+      <dt className="w-48">
         <FormattedMessage id={title} />
       </dt>
       {children}
