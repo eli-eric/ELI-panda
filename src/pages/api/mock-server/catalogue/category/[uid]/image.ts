6e9c41dfcc782b3ed883253a622ab4c0888469db
Jiri Svacha <jiri.svacha@eli-beams.eu>
2023-02-20 14:50:05 +0100

    fix mock server slug

--- src/pages/api/mock-server/catalogue/category/[uid]/image.ts
+++ src/pages/api/mock-server/catalogue/category/[uid]/image.ts
@@ -3,13 +3,21 @@ import fs from 'fs'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import path from 'path'
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<any>) {
-  const { slug } = req.query
-  if (slug && slug.length === 2 && slug[1] === 'image') {
-    const uid = slug[0]
-
-    let filePath = path.resolve('.', 'mock-data/images/catalogue/category/' + uid + '/image.png')
-    if (!fs.existsSync(filePath)) filePath = path.resolve('.', 'mock-data/images/catalogue/category/no-image.png')
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>,
+) {
+  const { uid } = req.query
+  if (uid) {
+    let filePath = path.resolve(
+      '.',
+      'mock-data/images/catalogue/category/' + uid + '/image.png',
+    )
+    if (!fs.existsSync(filePath))
+      filePath = path.resolve(
+        '.',
+        'mock-data/images/catalogue/category/no-image.png',
+      )
 
     const imageBuffer = fs.readFileSync(filePath)
 
