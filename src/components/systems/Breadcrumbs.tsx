5b561f86d518aeb23f7050ab7c2890ae4900cd00
Jiri Majer <juras.majer@gmail.com>
2023-02-20 10:18:30 +0100

    Fix keys in Breadcrumbs

--- src/components/systems/Breadcrumbs.tsx
+++ src/components/systems/Breadcrumbs.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import { Fragment } from 'react'
 import { fetchFakeSystems } from 'src/pages/tree/[slug]'
 import useSWR from 'swr'
 
@@ -14,10 +15,9 @@ const Breadcrumbs = (props: { path: string[] }) => {
         <span className="truncate">/</span>
       </div>
       {data?.map(({ uid, name }) => (
-        <>
+        <Fragment key={uid}>
           <Link
             className="text-gray-600 hover:bg-gray-50 hover:text-gray-900 flex items-center px-1 py-1 text-sm font-medium "
-            key={uid}
             href={`/tree/${uid}`}
           >
             <span className="truncate">{name}</span>
@@ -25,7 +25,7 @@ const Breadcrumbs = (props: { path: string[] }) => {
           <div className="text-gray-600 flex items-center py-1 text-sm font-medium ">
             <span className="truncate">/</span>
           </div>
-        </>
+        </Fragment>
       ))}
     </div>
   )
