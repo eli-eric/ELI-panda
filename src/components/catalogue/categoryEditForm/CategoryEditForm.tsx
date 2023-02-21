79b9c29709a2fd65a53deab8da480b8a4e2b9d4e
Jan Smrčka <jan.smrck@gmail.com>
2023-02-21 14:46:23 +0100

    better mutate

--- src/components/catalogue/categoryEditForm/CategoryEditForm.tsx
+++ src/components/catalogue/categoryEditForm/CategoryEditForm.tsx
@@ -19,7 +19,7 @@ interface Props {
 const CategoryEditForm = ({ uid, onSubmit, children }: Props) => {
   const endpoints = useEndpoint({ uid })
 
-  const { data, mutate } = useSWR<CatalogueFormType>(
+  const { data } = useSWR<CatalogueFormType>(
     uid && endpoints.catalogueCategoryEdit,
   )
 
@@ -50,13 +50,7 @@ const CategoryEditForm = ({ uid, onSubmit, children }: Props) => {
   return (
     <Fragment>
       <FormProvider {...formMethods}>
-        <form
-          className="flex"
-          onSubmit={formMethods.handleSubmit(data => {
-            onSubmit(data)
-            mutate()
-          })}
-        >
+        <form className="flex" onSubmit={formMethods.handleSubmit(onSubmit)}>
           <div className="flex-1">
             <Main uid={uid} />
             <GroupList />
