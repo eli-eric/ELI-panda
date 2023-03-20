f167d80f2e797ecf143abf9610bd7a9192fe9789
Jan Smrčka <jan.smrck@gmail.com>
2023-03-20 15:49:30 +0100

    repair forms in systems and catalogue

--- src/modules/systems/components/Breadcrumbs.tsx
+++ src/modules/systems/components/Breadcrumbs.tsx
@@ -3,21 +3,16 @@ import { Fragment } from 'react'
 import BreadcrumpContainer from '@/components/Breadcrump/Breadcrump.cont'
 import BreadcrumpItem from '@/components/Breadcrump/Breadcrump.item'
 
-import { useSystemEdit } from '../hooks/useSystemEdit'
 import { ParentPath } from '../types/responses'
 
-const Breadcrumbs = ({ parentPath }: { parentPath?: ParentPath }) => {
-  const { AddButton } = useSystemEdit({})
-  return (
-    <BreadcrumpContainer homeLink="/systems">
-      <Fragment>
-        {parentPath?.map(({ uid, name }) => (
-          <BreadcrumpItem key={uid} name={name} link={'/systems/' + uid} />
-        ))}
-        {/* <AddButton /> */}
-      </Fragment>
-    </BreadcrumpContainer>
-  )
-}
+const Breadcrumbs = ({ parentPath }: { parentPath?: ParentPath }) => (
+  <BreadcrumpContainer homeLink="/systems">
+    <Fragment>
+      {parentPath?.map(({ uid, name }) => (
+        <BreadcrumpItem key={uid} name={name} link={'/systems/' + uid} />
+      ))}
+    </Fragment>
+  </BreadcrumpContainer>
+)
 
 export default Breadcrumbs
