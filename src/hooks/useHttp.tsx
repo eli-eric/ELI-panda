e7959573dcefe46fffe9fc70277f3e64e293a6d6
Jiri Majer <juras.majer@gmail.com>
2023-04-21 13:23:30 +0200

    Init fetch hook

---
+++ src/hooks/useHttp.tsx
@@ -0,0 +1,59 @@
+import { useCallback, useState } from 'react'
+
+type RequestOptions = {
+  method?: string
+  headers?: HeadersInit
+  body?: BodyInit | null
+  mode?: RequestMode
+  credentials?: RequestCredentials
+  cache?: RequestCache
+  redirect?: RequestRedirect
+  referrer?: string
+  referrerPolicy?: ReferrerPolicy
+  integrity?: string
+  keepalive?: boolean
+  signal?: AbortSignal | null
+  window?: any
+}
+
+const useHttp = () => {
+  const initialState = {
+    isLoading: false,
+    data: null,
+    error: ''
+  }
+
+  const [state, setState] = useState(initialState)
+
+  const executeRequest = useCallback(
+    (methodOptions: RequestOptions) =>
+      async (url: string, options: RequestOptions = {}) => {
+        const requestOptions = { ...options, ...methodOptions }
+        const request = new Request(url, requestOptions)
+
+        setState(prevState => ({ ...prevState, isLoading: true }))
+
+        try {
+          const response = await fetch(request)
+          const jsonData = await response.json()
+          setState(prevState => ({ ...prevState, isLoading: false, data: jsonData }))
+        } catch (err) {
+          const errorMessage = (err as Error).toString()
+          setState({ error: errorMessage, data: null, isLoading: false })
+        }
+      },
+    []
+  )
+
+  const httpMethods = {
+    get: executeRequest({ method: 'get' }),
+    post: executeRequest({ method: 'post' }),
+    put: executeRequest({ method: 'put' }),
+    patch: executeRequest({ method: 'patch' }),
+    delete: executeRequest({ method: 'delete' })
+  }
+
+  return { ...state, fetch: httpMethods }
+}
+
+export default useHttp
