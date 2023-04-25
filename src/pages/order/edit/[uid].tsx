3580e51dd016cae80d047d72c7be676571659467
Jan Smrčka <jan.smrck@gmail.com>
2023-04-25 15:49:27 +0200

    added pages structure

--- src/pages/order/edit/[uid].tsx
+++ src/pages/order/edit/[uid].tsx
@@ -18,7 +18,7 @@ const OrderItemPage: NextPage = (): JSX.Element => {
       <main className="flex-1">
         <div className="py-6">
           <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
-            <h1 className="text-2xl font-semibold text-gray-900">ORDER ITEM</h1>
+            <h1 className="text-2xl font-semibold text-gray-900">ORDER EDIT</h1>
           </div>
           <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
             {/* Replace with your content */}
