8422d7d4462fabad29bb9b242a0fe4782a22493e
Jiri Majer <juras.majer@gmail.com>
2023-04-26 18:24:17 +0200

    Handle empty OK response

--- src/helpers/executeRequest.tsx
+++ src/helpers/executeRequest.tsx
@@ -1,8 +1,10 @@
 type SuccessCallback<T> = (result: T) => void
 type ErrorCallback = (err: Error) => void
 
+const defaultHeaders = new Headers({ 'Content-Type': 'application/json' })
 const defaultOptions: RequestInit = {
   // mode: 'no-cors'
+  headers: defaultHeaders
 }
 
 async function executeRequest<T>(
@@ -15,8 +17,13 @@ async function executeRequest<T>(
   const request = new Request(url, requestOptions)
   try {
     const response = await fetch(request)
-    const jsonData = await response.json()
-    onSuccess(jsonData)
+    try {
+      const jsonData = await response.json()
+      onSuccess(jsonData)
+    } catch (e) {
+      //handle cases where response is OK but doesn't contain a JSON payload
+      onSuccess({} as T)
+    }
   } catch (err) {
     if (err instanceof Error) {
       onError(err)
