6a86c94a3b2d1fc41ef7aeab264ee5e6a2f25383
Jiri Majer <juras.majer@gmail.com>
2023-04-26 18:26:04 +0200

    Derive file params from router rather than props

--- src/modules/fileManager/FileManager.tsx
+++ src/modules/fileManager/FileManager.tsx
@@ -1,141 +1,110 @@
-import { PencilSquareIcon, TrashIcon } from '@heroicons/react/24/outline'
-import { useMemo, useState } from 'react'
-import { toast } from 'react-hot-toast'
-import useGeneralTable from 'src/hooks/useGeneralTable' // Assuming the hook is in the same folder
+import { TrashIcon } from '@heroicons/react/24/outline'
+import { nanoid } from 'nanoid'
+import { useRouter } from 'next/router'
+import { useCallback, useEffect, useMemo, useState } from 'react'
+import { useDropzone } from 'react-dropzone'
+import useGeneralTable from 'src/hooks/useGeneralTable'
+import useSWR from 'swr'
 
-// Assuming the hook is in the same folder
 import { Button } from '@/components/Buttons'
 import executeRequest from '@/helpers/executeRequest'
+import { uniFetcher } from '@/helpers/fetcher'
 
-type FileItem = {
+export type FileItem = {
   id: string
   name: string
   type: string
   url: string
 }
 
-type FileManagerProps = {
-  type: 'catalogue' | 'systems'
-  id: string
-}
+const FileManager = () => {
+  const router = useRouter()
 
-const error = null
-const files: FileItem[] = [
-  {
-    id: '1',
-    name: 'example1.pdf',
-    type: 'application/pdf',
-    url: 'https://example.com/files/example1.pdf'
-  },
-  {
-    id: '2',
-    name: 'example2.jpg',
-    type: 'image/jpeg',
-    url: 'https://example.com/files/example2.jpg'
-  },
-  {
-    id: '3',
-    name: 'example3.mp4',
-    type: 'video/mp4',
-    url: 'https://example.com/files/example3.mp4'
-  },
-  {
-    id: '4',
-    name: 'example4.docx',
-    type: 'application/pdf',
-    url: 'https://example.com/files/example4.docx'
-  },
-  {
-    id: '5',
-    name: 'example5.png',
-    type: 'image/png',
-    url: 'https://example.com/files/example5.png'
-  }
-]
+  const [, itemType] = router.pathname.split('/')
+  const { uid: itemId } = router.query
 
-const FileManager = ({ type, id }: FileManagerProps) => {
-  const endpoint = `/api/${type}/${id}/files`
-  // const { data: files, error } = useSWR<Array<FileItem>>(FILE_ENDPOINT)
+  const endpoint = `http://localhost:5001/api/${itemType}/${itemId}/files`
+  const {
+    data: files,
+    error,
+    mutate
+  } = useSWR<Array<FileItem>>(endpoint, uniFetcher)
 
-  const [newFile, setNewFile] = useState('')
+  const [newFile, setNewFile] = useState({ name: '', payload: '' })
 
-  const handleUpload = async e => {
-    const file = e.target.files[0]
+  const onDrop = useCallback(async files => {
+    const file = files[0]
     const reader = new FileReader()
     reader.readAsDataURL(file)
-    reader.onload = () => setNewFile(String(reader.result))
-  }
+    reader.onload = () =>
+      setNewFile({ name: file.name, payload: String(reader.result) })
+  }, [])
 
-  const openForm = (id: string) => {}
+  const handlePost = useCallback(
+    (name: string, payload: string) => {
+      const body = JSON.stringify({ name, payload })
+      executeRequest<FileItem>(endpoint, { method: 'post', body }, res => {
+        mutate([...(files ?? []), res])
+        setNewFile({ name: '', payload: '' })
+      })
+    },
+    [endpoint, mutate, files, setNewFile]
+  )
 
-  const handlePost = (name: string, payload: string) => {
-    const body = JSON.stringify({ name, payload })
-    executeRequest<FileItem>(
-      endpoint,
-      { method: 'post', body },
-      res => toast.success(`Succesfully uploaded ${res.name}!`),
-      () => toast.error('oops')
-    )
-  }
+  const handleDelete = useCallback(
+    (id: string) =>
+      executeRequest(`${endpoint}/${id}`, { method: 'delete' }, () => {
+        mutate((files ?? []).filter(obj => obj.id !== id))
+      }),
+    [endpoint, files, mutate]
+  )
 
-  const handlePatch = (id: string, newName: string) => {
-    const body = JSON.stringify({ name: newName })
-    executeRequest(
-      `${endpoint}/${id}`,
-      { method: 'patch', body },
-      () => toast.success(`You have renamed a file, wow`),
-      () => toast.error('oops')
-    )
-  }
-
-  const handleDelete = (id: string) =>
-    executeRequest(
-      `${endpoint}/${id}`,
-      { method: 'delete' },
-      () => toast.success('Deleted.'),
-      () => toast.error('oops')
-    )
+  useEffect(() => {
+    newFile.name && newFile.payload && handlePost(newFile.name, newFile.payload)
+  }, [newFile, handlePost])
 
   // Define columns for useGeneralTable
   const columns = useMemo(
     () => [
       {
         Header: 'Name',
-        accessor: 'name'
-      },
-      {
-        Header: 'Type',
-        accessor: 'type'
-      },
-      {
-        Header: 'URL',
-        accessor: 'url',
-        Cell: ({ value }) => (
-          <a href={value} target="_blank" rel="noreferrer">
-            {value}
-          </a>
-        )
+        accessor: 'name',
+        Cell: ({ row }) => {
+          const {
+            original: { name, url }
+          } = row
+          return (
+            <a href={url} target="_blank" rel="noreferrer">
+              {name}
+            </a>
+          )
+        }
       },
       {
         Header: 'Actions',
         accessor: 'id',
         Cell: ({ value }) => (
-          <div className="flex-row">
-            <Button onClick={() => openForm(value)}>
-              <PencilSquareIcon className="h-5 w-5 text-red-700" aria-hidden="true" />
-            </Button>
-            <Button onClick={() => handleDelete(value)}>
-              <TrashIcon className="h-5 w-5 text-red-700" aria-hidden="true" />
-            </Button>
-          </div>
+          <Button
+            onClick={() => {
+              handleDelete(value)
+            }}
+          >
+            <TrashIcon className="h-5 w-5 text-red-700" aria-hidden="true" />
+          </Button>
         )
       }
     ],
-    []
+    [handleDelete]
   )
 
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop
+  })
+
   // Use useGeneralTable hook
+  const tableId = nanoid()
   const { getTable } = useGeneralTable({
+    tableId,
     data: files,
     columns,
     loading: !error && !files
@@ -144,6 +113,12 @@ const FileManager = ({ type, id }: FileManagerProps) => {
   return (
     <div>
       {/* Your file upload UI here */}
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        <p className={isDragActive ? 'bg-orange-600' : ''}>
+          Drag new file here
+        </p>
+      </div>
       {getTable()}
     </div>
   )
