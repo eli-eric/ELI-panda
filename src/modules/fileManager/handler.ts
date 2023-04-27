e04e688c0a1b6a9d105e9d9ddc0003f261e6a9f5
Jiri Majer <juras.majer@gmail.com>
2023-04-27 12:23:56 +0200

    Add debug messages

--- src/modules/fileManager/handler.ts
+++ src/modules/fileManager/handler.ts
@@ -32,17 +32,21 @@ const makeFileItem = (s3Object: BucketItemWithMetadata): FileItem => {
   }
 }
 
+const composeDebugMessage = (req: NextApiRequest, message: string) =>
+  `Request URL: ${req.url} | Method: ${req.method} | ${message}`
+
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
   try {
     if (!req.url) {
+      logger.debug(composeDebugMessage(req, 'Request URL is missing'))
       res.status(400).end()
     } else {
       const [, , itemType, itemId, , fileId] = req.url.split('/')
 
       const prefix = `${itemType}/${itemId}`
 
-      logger.info(
-        `Request - URL: ${req.url} | Method: ${req.method} | Item Type: ${itemType} | Item ID: ${itemId} | FileID: ${fileId}`
+      logger.debug(
+        composeDebugMessage(req, `Extracted attributes - ItemType: ${itemType} | ItemID: ${itemId} | FileID: ${fileId}`)
       )
 
       switch (req.method) {
@@ -57,10 +61,14 @@ const handler = (req: NextApiRequest, res: NextApiResponse) => {
 
           stream.on('error', err => {
             logger.error('Error listing bucket', err)
+            logger.debug(composeDebugMessage(req, 'Failed to list objects in the bucket'))
             res.status(500).end()
           })
 
-          stream.on('end', () => res.status(200).json(objects))
+          stream.on('end', () => {
+            logger.debug(composeDebugMessage(req, 'Successfully listed objects in the bucket'))
+            res.status(200).json(objects)
+          })
 
           break
 
@@ -83,15 +91,21 @@ const handler = (req: NextApiRequest, res: NextApiResponse) => {
             s3Client.putObject(bucket, `${prefix}/${id}`, buffer, buffer.length, metaData, err => {
               if (err) {
                 logger.error('Error saving file', err)
+                logger.debug(composeDebugMessage(req, 'Failed to save file'))
                 res.status(500).end()
               } else {
                 s3Client.statObject(bucket, `${prefix}/${id}`, (err, obj) => {
-                  if (err) return res.status(500).end()
+                  if (err) {
+                    logger.debug(composeDebugMessage(req, 'Failed to get file info after saving'))
+                    return res.status(500).end()
+                  }
+                  logger.debug(composeDebugMessage(req, 'Successfully saved file'))
                   res.status(201).json(makeFileItem(makeBucketItem(obj, id, prefix)))
                 })
               }
             })
           } else {
+            logger.debug(composeDebugMessage(req, 'Invalid base64 payload in request body'))
             res.status(400).end()
           }
 
@@ -103,19 +117,23 @@ const handler = (req: NextApiRequest, res: NextApiResponse) => {
             s3Client.statObject(bucket, fullPath, (err, result) => {
               if (err) {
                 logger.error('Error finding object to delete', err)
+                logger.debug(composeDebugMessage(req, 'Failed to find object to delete'))
                 return res.status(500).end()
               } else {
                 s3Client.removeObject(bucket, fullPath, err => {
                   if (err) {
                     logger.error('Error deleting file', err)
-                    return res.status(500).end()
+                    logger.debug(composeDebugMessage(req, 'Failed to delete file'))
+                    return res.status(500).end
                   } else {
+                    logger.debug(composeDebugMessage(req, 'Successfully deleted file'))
                     res.status(200).json(makeBucketItem(result, fileId, prefix))
                   }
                 })
               }
             })
           } else {
+            logger.debug(composeDebugMessage(req, 'File ID is missing in the request URL for deletion'))
             res.status(400).end()
           }
 
@@ -123,10 +141,12 @@ const handler = (req: NextApiRequest, res: NextApiResponse) => {
 
         default:
           res.setHeader('Allow', ['GET', 'POST', 'DELETE'])
+          logger.debug(composeDebugMessage(req, `Method ${req.method} Not Allowed`))
           res.status(405).end(`Method ${req.method} Not Allowed`)
       }
     }
   } catch (error) {
+    logger.debug(composeDebugMessage(req, 'An error occurred during request handling'))
     res.status(500).end()
     logger.error(error)
   }
