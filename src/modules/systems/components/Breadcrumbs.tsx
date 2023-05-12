c7914205f0b9584308de1d2b3f297ff2c2e71f2d
bartosjiri <me@bartosjiri.com>
2023-05-12 14:06:10 +0200

    Update breadcrumbs component

--- src/modules/systems/components/Breadcrumbs.tsx
+++ src/modules/systems/components/Breadcrumbs.tsx
@@ -1,5 +1,3 @@
-import { Fragment } from 'react'
-
 import BreadcrumpContainer from '@/components/Breadcrump/Breadcrump.cont'
 import BreadcrumpItem from '@/components/Breadcrump/Breadcrump.item'
 
@@ -7,11 +5,9 @@ import type { ParentPath } from '../types/responses'
 
 const Breadcrumbs = ({ parentPath }: { parentPath?: ParentPath }) => (
   <BreadcrumpContainer homeLink="/systems">
-    <Fragment>
-      {parentPath?.map(({ uid, name }) => (
-        <BreadcrumpItem key={uid} name={name} link={'/systems/' + uid} />
-      ))}
-    </Fragment>
+    {parentPath?.map(({ uid, name }) => (
+      <BreadcrumpItem key={uid} name={name} link={'/systems/' + uid} />
+    ))}
   </BreadcrumpContainer>
 )
 
