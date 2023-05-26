f1fa4b32cc72a86a6cdad0ab3ce5b73c97ff6849
Jan Smrčka <jan.smrck@gmail.com>
2023-05-26 11:20:59 +0200

    fix datepicker behaviour, some refactors

---
+++ src/modules/shared/fileManager/FileActions.tsx
@@ -0,0 +1,62 @@
+import Link from 'next/link'
+import { useCallback } from 'react'
+import { toast } from 'react-hot-toast'
+import { useIntl } from 'react-intl'
+import type { KeyedMutator } from 'swr'
+
+import { DeleteButton, DownloadButton } from '@/components/Buttons'
+import executeRequest from '@/helpers/executeRequest'
+import { createMessageValues } from '@/helpers/formatters'
+import useWarningModal from '@/hooks/useWarningModal'
+import { message } from '@/i18n/src/messages'
+
+import type { FileItem } from './types'
+
+const messages = message.common.fileManager
+
+interface FileActionsProps {
+  file: FileItem
+  endpoint: string
+  files?: FileItem[]
+  mutate: KeyedMutator<FileItem[]>
+
+  hasEditRole?: boolean
+}
+
+const FileActions = ({ file, endpoint, files, mutate, hasEditRole }: FileActionsProps) => {
+  const intl = useIntl()
+
+  const handleDelete = useCallback(
+    (id: string) => {
+      const name = (files ?? []).find(obj => obj.id === id)?.name
+      executeRequest(
+        `${endpoint}/${id}`,
+        { method: 'delete' },
+        () => {
+          toast.success(`Deleted ${name}`)
+          mutate((files ?? []).filter(obj => obj.id !== id))
+        },
+        () => toast.error(`Failed to delete ${name}`)
+      )
+    },
+    [endpoint, files, mutate]
+  )
+
+  const { withWarningModal, WarningModal } = useWarningModal(
+    intl.formatMessage({ id: messages.deleteModal.text }, createMessageValues({ fileName: file.name }))
+  )
+
+  return (
+    <div className="py-1">
+      <Link href={file.url} passHref legacyBehavior={true}>
+        <a target="_blank">
+          <DownloadButton className="mr-1" />
+        </a>
+      </Link>
+      {hasEditRole && <DeleteButton onClick={() => withWarningModal(handleDelete)(file.id)} />}
+      <WarningModal />
+    </div>
+  )
+}
+
+export default FileActions
