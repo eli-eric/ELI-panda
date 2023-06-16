18e7e120ec6cf79537aa64adc1053bd9046b6472
Jiri Majer <juras.majer@gmail.com>
2023-06-17 01:02:49 +0200

    Integrate imange manager in catalogue item form

--- src/modules/catalogueItem/Item.cont.tsx
+++ src/modules/catalogueItem/Item.cont.tsx
@@ -1,4 +1,5 @@
 import { DevTool } from '@hookform/devtools'
+import { useRouter } from 'next/router'
 import { Suspense } from 'react'
 import { ErrorBoundary } from 'react-error-boundary'
 import { FormProvider } from 'react-hook-form'
@@ -7,22 +8,27 @@ import ErrorPage from '@/components/error/ErrorPage'
 import { TextArea } from '@/components/form/Input'
 import Card from '@/components/layout/Card'
 import ProgressBarComponent from '@/components/progress-bar.comp'
+import useImageManager from '@/hooks/useImageManager'
+import { FILE_TYPE } from '@/types/constants/files'
 
-import ImageGalleryComponent from '../../components/item-detail/ImageGallery'
 import DefaultItemForm from './components/form/DefaultItemForm'
 import Groups from './components/form/Groups'
 import ItemHeader from './components/header/Header.comp'
-import useItem from './hooks/useItem'
 import useItemForm from './hooks/useItemForm'
 import useItemSubmit from './hooks/useItemSubmit'
 
 const ItemContainer = () => {
   const { FormWarningModal, ...formMethods } = useItemForm()
-  const { image } = useItem()
   const { submit, loading } = useItemSubmit()
+  const { query } = useRouter()
+  const { save: saveImages, Gallery: ImageGallery } = useImageManager({
+    itemCategory: FILE_TYPE.CATALOGUE,
+    itemId: String(query.uid)
+  })
 
   const onSubmit = (data: any) => {
     submit(data)
+    saveImages()
   }
 
   return (
@@ -31,8 +37,9 @@ const ItemContainer = () => {
         <ItemHeader disabledEdit={false} loading={loading} />
         <Card className="flex flex-col justify-between pb-5">
           <div className="lg:grid lg:grid-cols-3 lg:items-start lg:gap-x-8 pb-3">
-            {/* //TODO: image upload will replace ImageGalleryComponent */}
-            <ImageGalleryComponent images={[image]} />
+            <div className="pt-6 pl-6">
+              <ImageGallery hasEditRole={true} />
+            </div>
             <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0 col-span-2">
               <DefaultItemForm />
             </div>
