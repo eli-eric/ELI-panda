a9b99123a388030cf5548c36384c1c9bb8549477
Jiri Majer <juras.majer@gmail.com>
2023-06-19 14:45:27 +0200

    Remove missing Image Manager

--- src/components/item-detail/ItemDetail.comp.tsx
+++ src/components/item-detail/ItemDetail.comp.tsx
@@ -1,10 +1,7 @@
 import type { StaticImageData } from 'next/image'
-import { useRouter } from 'next/router'
 
 import { Grid } from '@/components/grid/Grid'
-import useImageManager from '@/hooks/useImageManager'
 import { message } from '@/i18n/src/messages'
-import { FILE_TYPE } from '@/types/constants/files'
 
 import ItemPropertyTitle from '../item-property/item-property-title.comp'
 import ItemPropertyValue from '../item-property/item-property-value.comp'
@@ -18,18 +15,9 @@ interface Props {
 
 const messages = message.common.property
 
-const ItemDetailComponent = ({ title, images, description, children }: Props) => {
-  const router = useRouter()
-  const { save, Gallery: ImageGallery } = useImageManager({
-    itemCategory: FILE_TYPE.SYSTEM,
-    itemId: String(router.query.uid)
-  })
-  return (
+const ItemDetailComponent = ({ title, images, description, children }: Props) => (
     <Grid className="pb-10">
-      <div className="col-span-3 md:col-span-2 lg:col-span-4 mr-auto pr-4">
-        <ImageGallery hasEditRole={true} />
-        <button onClick={() => save()}>yallah</button>
-      </div>
+      <div className="col-span-3 md:col-span-2 lg:col-span-4 mr-auto pr-4"></div>
       <div className="col-span-3 md:col-span-4 lg:col-span-8">
         <h1 className="text-xl font-bold tracking-tight text-gray-900 mb-4 mt-4 md:mt-0">{title}</h1>
         <dl className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">{children}</dl>
@@ -43,6 +31,5 @@ const ItemDetailComponent = ({ title, images, description, children }: Props) =>
       )}
     </Grid>
   )
-}
 
 export default ItemDetailComponent
