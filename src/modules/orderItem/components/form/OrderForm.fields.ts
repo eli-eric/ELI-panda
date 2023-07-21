ff0c211a5e3f37f1fd034233cac3b4c2b328735c
Jan Smrčka <jan.smrck@gmail.com>
2023-07-21 16:55:39 +0200

    refactor order field values

--- src/modules/orderItem/components/form/OrderForm.fields.ts
+++ src/modules/orderItem/components/form/OrderForm.fields.ts
@@ -1,5 +1,6 @@
 import { useMakeFormFields } from '@/hooks/form/useMakeFormFields'
 import { message } from '@/i18n/src/messages'
+import { CODEBOOK } from '@/types/constants/codebook'
 
 import useOrderDetail from '../../hooks/useOrderDetail'
 
@@ -7,83 +8,80 @@ import useOrderDetail from '../../hooks/useOrderDetail'
 const { form } = message.ordersPage.orderDetail
 
 const useOrderFormFields = () => {
-  const { disabledEdit: disabled } = useOrderDetail()
+  const { disabledEdit } = useOrderDetail()
 
   return useMakeFormFields({
     name: {
       name: 'name',
       label: form.name.label,
       placeholder: form.name.placeholder,
-      disabled: disabled,
+      disabled: disabledEdit,
       rounded: 'rounded-md'
     },
     orderNumber: {
       name: 'orderNumber',
       label: form.orderNumber.label,
       placeholder: form.orderNumber.placeholder,
-      disabled: disabled,
+      disabled: disabledEdit,
       rounded: 'rounded-md'
     },
     requestNumber: {
       name: 'requestNumber',
       label: form.requestNumber.label,
       placeholder: form.requestNumber.placeholder,
-      disabled: disabled,
+      disabled: disabledEdit,
       rounded: 'rounded-md'
     },
     contractNumber: {
       name: 'contractNumber',
       label: form.contractNumber.label,
       placeholder: form.contractNumber.placeholder,
-      disabled: disabled,
+      disabled: disabledEdit,
       rounded: 'rounded-md'
     },
-    // supplier: {
-    //   name: 'supplier',
-    //   label: form.supplier.label,
-    //   placeholder: form.supplier.placeholder,
-    //   disabled: disabled,
-    //   isError: !!formState.errors.supplier,
-    //   rounded: 'rounded-md',
-    //   codebook: CODEBOOK.SUPPLIER
-    // },
-    // procurementResponsible: {
-    //   name: 'procurementResponsible',
-    //   label: form.procurementResponsible.label,
-    //   disabled: disabled,
-    //   isError: !!formState.errors.procurementResponsible,
-    //   rounded: 'rounded-md',
-    //   emptyOption: true,
-    //   codebook: CODEBOOK.PROCUREMENTER
-    // },
-    // requestor: {
-    //   name: 'requestor',
-    //   label: form.requestor.label,
-    //   placeholder: form.requestor.placeholder,
-    //   disabled: disabled,
-    //   isError: !!formState.errors.requestor,
-    //   rounded: 'rounded-md',
-    //   codebook: CODEBOOK.EMPLOYEE
-    // },
-    // orderStatus: {
-    //   name: 'orderStatus',
-    //   label: form.orderStatus.label,
-    //   disabled: disabled,
-    //   isError: !!formState.errors.orderStatus,
-    //   rounded: 'rounded-md',
-    //   codebook: CODEBOOK.ORDER_STATUS
-    // },
+    supplier: {
+      name: 'supplier',
+      label: form.supplier.label,
+      placeholder: form.supplier.placeholder,
+      disabled: disabledEdit,
+      rounded: 'rounded-md',
+      codebook: CODEBOOK.SUPPLIER
+    },
+
+    procurementResponsible: {
+      name: 'procurementResponsible',
+      label: form.procurementResponsible.label,
+      disabled: disabledEdit,
+      rounded: 'rounded-md',
+      emptyOption: true,
+      codebook: CODEBOOK.PROCUREMENTER
+    },
+    requestor: {
+      name: 'requestor',
+      label: form.requestor.label,
+      placeholder: form.requestor.placeholder,
+      disabled: disabledEdit,
+      rounded: 'rounded-md',
+      codebook: CODEBOOK.EMPLOYEE
+    },
+    orderStatus: {
+      name: 'orderStatus',
+      label: form.orderStatus.label,
+      disabled: disabledEdit,
+      rounded: 'rounded-md',
+      codebook: CODEBOOK.ORDER_STATUS
+    },
     notes: {
       name: 'notes',
       label: form.notes.label,
-      disabled: disabled,
+      disabled: disabledEdit,
       rounded: 'rounded-md'
     },
     orderDate: {
       name: 'orderDate',
       label: form.orderDate.label,
       type: 'date',
-      disabled: disabled,
+      disabled: disabledEdit,
       rounded: 'rounded-md'
     }
   })
