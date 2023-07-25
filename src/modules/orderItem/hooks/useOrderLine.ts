c27684371ca766f272a6e600e0bffcea96e23051
Jan Smrčka <jan.smrck@gmail.com>
2023-07-25 09:52:22 +0200

    fix order lines eun

--- src/modules/orderItem/hooks/useOrderLine.ts
+++ src/modules/orderItem/hooks/useOrderLine.ts
@@ -1,20 +1,20 @@
 import { useFieldArray, useFormContext } from 'react-hook-form'
 import uuid from 'react-uuid'
 
-import type { OrderLineFormType } from '../types/form'
+import type { OrderDetailFormType, OrderLineFormType } from '../types/form'
 
 export const useOrderLine = () => {
-  const { control } = useFormContext()
+  const { control } = useFormContext<OrderDetailFormType>()
   const { insert, update, fields, remove } = useFieldArray({ control, name: 'orderLines' })
 
   //  set the order lines to the form
   const setOrderLine = (orderLine: OrderLineFormType) => {
     const dataToSave = { ...orderLine }
-    if (orderLine.id) {
-      const index = fields.findIndex(item => item.id === orderLine.id)
+    if (orderLine.uuid) {
+      const index = fields.findIndex(item => item.uuid === orderLine.uuid)
       update(index, dataToSave)
     } else {
-      dataToSave.id = uuid()
+      dataToSave.uuid = uuid()
       insert(fields.length, dataToSave)
     }
   }
