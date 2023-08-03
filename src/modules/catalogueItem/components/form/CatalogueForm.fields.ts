d78fb1bb6a37f08c72526d332700d38eec8d2671
Jan Smrčka <jan.smrck@gmail.com>
2023-08-03 10:13:28 +0200

    fix catalogue edit permission

--- src/modules/catalogueItem/components/form/CatalogueForm.fields.ts
+++ src/modules/catalogueItem/components/form/CatalogueForm.fields.ts
@@ -1,31 +1,33 @@
 import { useMakeFormFields } from '@/hooks/form/useMakeFormFields'
+import usePermission from '@/hooks/usePermission'
 import { message } from '@/i18n/src/messages'
 import { CODEBOOK } from '@/types/constants/codebook'
+import { ROLE } from '@/types/constants/roles'
 
 // messages
 const { form } = message.cataloguePage.itemDetail
 
 const useCatalogueFormFields = () => {
-  const disabled = true
+  const disabled = !usePermission([ROLE.CATALOGUE_EDIT])
   return useMakeFormFields({
     name: {
       name: 'name',
       label: form.name.label,
       placeholder: form.name.placeholder,
-      disabled: disabled,
+      disabled,
       rounded: 'rounded-md'
     },
     catalogueNumber: {
       name: 'catalogueNumber',
       label: form.catalogueNumber.label,
       placeholder: form.catalogueNumber.placeholder,
-      disabled: disabled,
+      disabled,
       rounded: 'rounded-md'
     },
     category: {
       name: 'category',
       label: form.category.label,
-      disabled: disabled,
+      disabled,
       rounded: 'rounded-md',
       codebook: CODEBOOK.CATALOGUE_CATEGORY
     },
@@ -33,7 +35,7 @@ const useCatalogueFormFields = () => {
       name: 'supplier',
       label: form.manufacturer.label,
       placeholder: form.manufacturer.placeholder,
-      disabled: disabled,
+      disabled,
       rounded: 'rounded-md',
       codebook: CODEBOOK.SUPPLIER
     },
@@ -41,14 +43,14 @@ const useCatalogueFormFields = () => {
       name: 'manufacturerUrl',
       label: form.manuFacturerUrl.label,
       placeholder: form.manuFacturerUrl.placeholder,
-      disabled: disabled,
+      disabled,
       rounded: 'rounded-md'
     },
     description: {
       name: 'description',
       label: form.description.label,
       placeholder: form.description.placeholder,
-      disabled: disabled,
+      disabled,
       rounded: 'rounded-md'
     }
   })
