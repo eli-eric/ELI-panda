507bc232d23b26f631a0a636229ed0484e54d60b
Jan Smrčka <jan.smrck@gmail.com>
2023-09-01 18:33:36 +0200

    advanced systems mutations

--- src/modules/systems/components/SearchBarButtons.tsx
+++ src/modules/systems/components/SearchBarButtons.tsx
@@ -1,20 +1,43 @@
 import { useRouter } from 'next/router'
+import { toast } from 'react-hot-toast'
 
 import { PlusButton, RefreshButton } from '@/components/Buttons'
+import axiosInstance from '@/core/axios/axiosInstance'
 import usePermission from '@/hooks/usePermission'
+import { BASE_URL } from '@/types/constants/common'
 import { PATH } from '@/types/constants/paths'
 import { ROLE } from '@/types/constants/roles'
 
 import { useSystems } from '../hooks/useSystems'
+import type { SystemDetail } from '../types/responses'
 
 export const SearchBarButtons = () => {
   const canEdit = usePermission([ROLE.SYSTEM_EDIT])
-  const { mutate } = useSystems('systems')
+  const { mutate: systemsMutate } = useSystems('systems')
 
   const router = useRouter()
 
   const handleRefresh = () => {
-    mutate()
+    systemsMutate(
+      async systems => {
+        try {
+          const newSystems = await axiosInstance.get(BASE_URL + '/systems' + '?pagination={"page":1,"pageSize":50}')
+          const newSubSystems = await Promise.all<SystemDetail[]>(
+            newSystems?.data?.data?.map(async system => {
+              const newSubSystem = await axiosInstance.get<SystemDetail[]>(
+                BASE_URL + '/system/' + system.uid + '/subsystems'
+              )
+              return { ...system, subSystems: newSubSystem.data }
+            })
+          )
+          return { ...newSystems.data, data: newSubSystems }
+        } catch (error) {
+          toast.error('Error refreshing systems')
+          return systems
+        }
+      },
+      { revalidate: false }
+    )
   }
 
   const handleAdd = () => {
