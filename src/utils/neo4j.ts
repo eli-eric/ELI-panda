924b1a41e4c6b0d97e2b8a54731bdab1b695da1e
Jan Smrčka <jan.smrck@gmail.com>
2023-09-26 15:55:25 +0200

    fix neo4j password secret

--- src/utils/neo4j.ts
+++ src/utils/neo4j.ts
@@ -13,7 +13,7 @@ export default function getDriver() {
   const { uri, username, password } = defaultOptions
 
   if (!uri || !username || !password) {
-    throw new Error(`Neo4j connection details are missing or incorrect: password length ${password.length}`)
+    throw new Error(`Neo4j connection details are missing or incorrect.`)
   }
 
   try {
@@ -21,7 +21,7 @@ export default function getDriver() {
       driver = neo4j.driver(uri, neo4j.auth.basic(username, password))
     }
   } catch (error) {
-    console.error('Error creating Neo4j driver:', error)
+    console.error('Error creating Neo4j driver.', error)
     throw error // re-throw the error so that calling code knows something went wrong
   }
 
