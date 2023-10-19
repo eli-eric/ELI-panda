9b3808a153c5ae8617ea8b8bb24753cfe39ddc88
JanSmrcka <jan.smrck@gmail.com>
2023-10-19 13:09:47 +0200

    publis graphql dev tools for dev env

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,11 +1,12 @@
+import { neoSchema } from '@/server/apollo/schema'
 import { ApolloServer } from '@apollo/server'
+import { ApolloServerPluginLandingPageDisabled } from '@apollo/server/plugin/disabled'
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default'
 import { startServerAndCreateNextHandler } from '@as-integrations/next'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import type { NextAuthOptions } from 'next-auth'
 import { getServerSession } from 'next-auth'
 import { getToken } from 'next-auth/jwt'
-
-import { neoSchema } from '@/server/apollo/schema'
 
 import { authOptions } from './auth/[...nextauth]'
 
@@ -13,7 +14,13 @@ const server = async (): Promise<ApolloServer> => {
   const schema = await neoSchema.getSchema()
 
   const apolloConfig = {
-    schema
+    schema,
+    plugins: [
+      // Install a landing page plugin based on NODE_ENV
+      process.env.PANDA_ENV === 'production' || process.env.PANDA_ENV === 'test'
+        ? ApolloServerPluginLandingPageDisabled()
+        : ApolloServerPluginLandingPageLocalDefault({ footer: false })
+    ]
   }
   return new ApolloServer(apolloConfig)
 }
@@ -21,10 +28,10 @@ const server = async (): Promise<ApolloServer> => {
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const session = await getServerSession(req, res, authOptions as NextAuthOptions)
 
-  /* if (!session?.user && process.env.PANDA_ENV !== 'localhost') {
+  if (!session?.user && (process.env.PANDA_ENV !== 'localhost' ?? process.env.PANDA_ENV !== 'dev')) {
     res.status(403).send('Authentication required.')
     return
-  } */
+  }
 
   return startServerAndCreateNextHandler(await server(), {
     context: async (req, res) => {
