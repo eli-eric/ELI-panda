84ece55a4db8d4048336bd283a9c18cacffdaf2b
JanSmrcka <jan.smrck@gmail.com>
2024-01-09 09:46:46 +0100

    modal with reader

---
+++ src/modules/shared/qrReader/hooks/useFindSystem.ts
@@ -0,0 +1,36 @@
+import { gql, useQuery } from '@apollo/client'
+import toast from 'react-hot-toast'
+
+import type { Query } from '@/types/gql/graphql'
+import { SYSTEM_DETAIL } from '@/utils/graphql/fragments'
+
+const GET_SYSTEM = gql`
+  ${SYSTEM_DETAIL}
+  query System($where: SystemWhere) {
+    systems(where: $where) {
+      ...SystemDetail
+    }
+  }
+`
+export const useFindSystem = (eun?: string) => {
+  const { data, error, loading } = useQuery<Query>(GET_SYSTEM, {
+    variables: {
+      where: {
+        physicalItem: {
+          eun_CONTAINS: eun
+        }
+      }
+    },
+    skip: !eun,
+    onError: error => {
+      toast.error('Something went wrong while fetching system detail: ' + error.message)
+    },
+    fetchPolicy: 'network-only'
+  })
+
+  return {
+    systemDetail: data?.systems[0],
+    loading: loading,
+    error
+  }
+}
