53b1eae85e50df6b87455492b405ed15c4a91e81
JanSmrcka <jan.smrck@gmail.com>
2024-01-16 09:32:53 +0100

    ELIPANDA-218 subsystems, hide relations

--- src/modules/systemItem/SystemItem.cont.tsx
+++ src/modules/systemItem/SystemItem.cont.tsx
@@ -1,36 +1,34 @@
-import { Fragment, Suspense, useContext } from 'react'
+import { Fragment, Suspense } from 'react'
 import { ErrorBoundary } from 'react-error-boundary'
 
 import ErrorPage from '@/components/error/ErrorPage'
 import Card from '@/components/layout/Card'
 import ProgressBarComponent from '@/components/progress-bar.comp'
 import usePermission from '@/hooks/usePermission'
-import { SystemDetailContext } from '@/pages/system/[uid]'
 import { FILE_TYPE } from '@/types/constants/files'
 import { ROLE } from '@/types/constants/roles'
 
 import FileManager from '../shared/fileManager/FileManager'
 import { SystemForm } from './components/form/SystemForm.cont'
-import RelationsSection from './components/relationsSection/RelationsSection'
+import { SubSystemsContainer } from './components/subsystems/SubSystems.cont'
 
 interface Props {
   uid?: string
 }
 
 export const SystemItemContainer = ({ uid }: Props) => {
-  const { systemDetail } = useContext(SystemDetailContext)
   const hasEditRole = usePermission([ROLE.SYSTEM_EDIT])
-
   return (
     <Fragment>
       <SystemForm />
       {uid && (
         <Card className="flex flex-col justify-between">
-          <ErrorBoundary fallback={<ErrorPage />}>
+          {/* <ErrorBoundary fallback={<ErrorPage />}>
             <Suspense fallback={<ProgressBarComponent />}>
               <RelationsSection systemName={systemDetail?.name} />
             </Suspense>
-          </ErrorBoundary>
+          </ErrorBoundary> */}
+          <SubSystemsContainer />
           <ErrorBoundary fallback={<ErrorPage />}>
             <Suspense fallback={<ProgressBarComponent />}>
               <FileManager itemType={FILE_TYPE.SYSTEM} uid={uid} hasEditRole={hasEditRole} />
