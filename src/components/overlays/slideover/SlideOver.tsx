a23811ced974521e1532ac01d3aa9fb26011fb73
Jiri Svacha <jiri.svacha@eli-beams.eu>
2024-01-19 06:53:23 +0100

    systems filter layout

--- src/components/overlays/slideover/SlideOver.tsx
+++ src/components/overlays/slideover/SlideOver.tsx
@@ -41,7 +41,7 @@ export const SlideOver: FC<PropsWithChildren<Props>> = ({ children, open, setOpe
               leaveFrom="translate-x-0"
               leaveTo="-translate-x-full"
             >
-              <Dialog.Panel className="pointer-events-auto w-screen max-w-md">
+              <Dialog.Panel className="pointer-events-auto w-screen md:max-w-xl max-w-md">
                 <div className="flex h-full flex-col divide-y divide-gray-200 bg-white shadow-xl">
                   <div className="flex min-h-0 flex-1 flex-col overflow-y-scroll py-6">
                     <div className="px-4 sm:px-6">
