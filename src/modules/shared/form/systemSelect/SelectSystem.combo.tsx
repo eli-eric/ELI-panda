1b081e194adfc8e5c42c3decf387ea20de3d669a
Jan Smrčka <jan.smrck@gmail.com>
2024-01-25 13:43:22 +0100

    systems table fix

--- src/modules/shared/form/systemSelect/SelectSystem.combo.tsx
+++ src/modules/shared/form/systemSelect/SelectSystem.combo.tsx
@@ -4,10 +4,10 @@ import { useFormContext } from 'react-hook-form'
 import Listbox from '@/components/form/Listbox'
 import { Modal } from '@/components/overlays/modal/modal.comp'
 import type { CodebookType } from '@/hooks/fetch/useCodebook'
+import { SystemsTable } from '@/modules/systems/components/table/Systems.table'
 import type { CODEBOOK } from '@/types/constants/codebook'
 import type { FieldProps, ModalButtons, Option } from '@/types/form'
-
-import { SelectSystemTable } from './components/SelectSystem.table'
+import { classNames } from '@/utils'
 
 export const SelectSystemComboBox = ({
   selectSystemField,
@@ -57,10 +57,19 @@ export const SelectSystemComboBox = ({
         isFilter={isFilter}
       />
       <Modal open={open} setOpen={setOpen} buttons={buttons}>
-        <SelectSystemTable
-          setSelectedSystem={setSelectedSystem}
-          tableId="systemSelect"
-          selectedSystem={selectedSystem}
+        <SystemsTable
+          tableId={'systemSelect'}
+          hideButtons={true}
+          className={'overflow-y-auto relative h-[423px]'}
+          settings={{
+            enableRowSelection: true
+          }}
+          getRowProps={row => ({
+            onClick: () => {
+              setSelectedSystem({ name: row.original.name, uid: row.original.uid })
+            },
+            className: classNames(selectedSystem?.uid === row.original.uid ? 'bg-primary-200' : '', 'cursor-pointer')
+          })}
         />
       </Modal>
     </Fragment>
