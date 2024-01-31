ff979fa261349f8a6af154494fdf592aa14e7358
Jiri Svacha <jiri.svacha@eli-beams.eu>
2024-01-31 22:32:41 +0100

    store dark theme using zustand with persit middleware

--- src/components/DarkModeButon.tsx
+++ src/components/DarkModeButon.tsx
@@ -1,41 +1,24 @@
 import { MoonIcon, SunIcon } from '@heroicons/react/24/solid'
-import { useEffect, useState } from 'react'
+
+import { useDarkModeStore } from '@/store/useDarkModeStore'
 
 const DarkModeButton = () => {
-  const [dm, setDm] = useState(false)
-
-  useEffect(() => {
-    if (localStorage && localStorage.theme) {
-      if (localStorage.theme === 'dark') {
-        setDm(true)
-        document.documentElement.classList.add('dark')
-      }
-    }
-  }, [])
-
-  useEffect(() => {
-    const htmlElement = document.documentElement
-    if (dm) {
-      htmlElement.classList.add('dark')
-    } else {
-      htmlElement.classList.remove('dark')
-    }
-    localStorage.theme = dm ? 'dark' : 'light'
-  }, [dm])
-
-  const toggleTheme = () => {
-    setDm(!dm)
-  }
+  const dms = useDarkModeStore()
+  dms.setStoredTheme()
 
   return (
     <button
       type="button"
-      className={`rounded-full bg-gray-400 p-1 text-${dm ? 'gray-900' : 'white'} shadow-sm hover:text-${
-        dm ? 'white' : 'gray-900'
+      className={`rounded-full bg-gray-400 p-1 text-${dms.isDark ? 'gray-900' : 'white'} shadow-sm hover:text-${
+        dms.isDark ? 'white' : 'gray-900'
       } focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600`}
-      onClick={toggleTheme}
+      onClick={dms.toggleDarkMode}
     >
-      {dm ? <SunIcon className="h-5 w-5" aria-hidden="true" /> : <MoonIcon className="h-5 w-5" aria-hidden="true" />}
+      {dms.isDark ? (
+        <SunIcon className="h-5 w-5" aria-hidden="true" />
+      ) : (
+        <MoonIcon className="h-5 w-5" aria-hidden="true" />
+      )}
     </button>
   )
 }
