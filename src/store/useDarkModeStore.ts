2a5fcfce70ec00de23d2297a9a571a5892e9d2c4
JanSmrcka <jan.smrck@gmail.com>
2024-02-02 11:54:15 +0100

    dark mode sessionstorage -> localstorage

--- src/store/useDarkModeStore.ts
+++ src/store/useDarkModeStore.ts
@@ -25,7 +25,7 @@ export const useDarkModeStore = create<DarkModeStore>()(
     }),
     {
       name: 'theme', // name of the item in the storage (must be unique)
-      storage: createJSONStorage(() => sessionStorage) // (optional) by default, 'localStorage' is used
+      storage: createJSONStorage(() => localStorage) // (optional) by default, 'localStorage' is used
     }
   )
 )
