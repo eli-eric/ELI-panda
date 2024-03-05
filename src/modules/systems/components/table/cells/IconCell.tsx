36fb6156feaa7ab6d5621f53ff0f22a1c3667ce6
JanSmrcka <jan.smrck@gmail.com>
2024-03-05 14:42:33 +0100

    systems icons by item usage

--- src/modules/systems/components/table/cells/IconCell.tsx
+++ src/modules/systems/components/table/cells/IconCell.tsx
@@ -1,13 +1,22 @@
-import { CogIcon } from '@heroicons/react/24/outline'
+import { CogIcon, CubeIcon } from '@heroicons/react/24/solid'
 import type { CellContext } from '@tanstack/react-table'
 import type { FC } from 'react'
 
+import { Tooltip } from '@/components/Tooltip'
+import { ITEM_USAGE } from '@/modules/systems/types/constants'
 import type { SystemDetail } from '@/modules/systems/types/responses'
 
 export const IconCell: FC<CellContext<SystemDetail, any>> = ({ row }) => {
-  return (
-    <div>
-      <CogIcon className="h-3 w-3 text-gray-500 dark:text-gray-200" />
-    </div>
-  )
+  switch (row.original.physicalItem?.itemUsage?.uid) {
+    case ITEM_USAGE.SPARE_PART: {
+      return (
+        <Tooltip content="Spare part">
+          <CogIcon className="h-4 w-4 text-red-500" />
+        </Tooltip>
+      )
+    }
+    case ITEM_USAGE.IN_SYSTEM_PART: {
+      return <CubeIcon className="h-4 w-4 text-cyan-500" />
+    }
+  }
 }
