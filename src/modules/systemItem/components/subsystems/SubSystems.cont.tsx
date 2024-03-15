63f5ffe9a6962f60b4c860c3d0d8b6b15b89f944
Jan Smrčka <jan.smrck@gmail.com>
2024-03-15 16:29:45 +0100

    ELIPANDA-256 add subsystem in system detail button

--- src/modules/systemItem/components/subsystems/SubSystems.cont.tsx
+++ src/modules/systemItem/components/subsystems/SubSystems.cont.tsx
@@ -1,8 +1,11 @@
+import Link from 'next/link'
 import { Fragment, useContext } from 'react'
 
+import { PlusButton } from '@/components/Buttons'
 import { Heading } from '@/components/layout/Heading'
 import { PandaTable } from '@/modules/shared/table/pandaTable/PandaTable'
 import { SystemDetailContext } from '@/pages/system/[uid]'
+import { PATH } from '@/types/constants/paths'
 
 import { useSubsystemsColumns } from './SubSystems.columns'
 
@@ -13,7 +16,18 @@ export const SubSystemsContainer = () => {
 
   return (
     <Fragment>
-      <Heading customText="Sub Systems" />
+      <Heading customText="Sub Systems">
+        <Link
+          href={{
+            pathname: PATH.SYSTEM,
+            query: {
+              parentUid: systemDetail.uid
+            }
+          }}
+        >
+          <PlusButton buttonSize="large" primary />
+        </Link>
+      </Heading>
       <PandaTable
         columns={columns}
         tableId={'subsystems'}
