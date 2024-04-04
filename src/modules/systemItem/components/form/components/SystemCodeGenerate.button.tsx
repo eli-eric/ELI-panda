f981c0ba9f1349f03f05ad5853dea64cb8e89def
JanSmrcka <jan.smrck@gmail.com>
2024-04-04 12:34:35 +0200

    generate system code

---
+++ src/modules/systemItem/components/form/components/SystemCodeGenerate.button.tsx
@@ -0,0 +1,33 @@
+import { Button } from '@/components/Buttons'
+import { useSystemCodeClear } from '@/modules/systemItem/hooks/useSystemCodeClear'
+import { useSystemCodeGenerate } from '@/modules/systemItem/hooks/useSystemCodeGenerate'
+
+export const SystemCodeButton = () => {
+  const { loading, getSystemCode, disabled } = useSystemCodeGenerate()
+  const { clearSystemCode } = useSystemCodeClear()
+
+  const handleGenerate = () => {
+    getSystemCode()
+  }
+
+  const handleClear = () => {
+    clearSystemCode()
+  }
+
+  return (
+    <div className="flex">
+      <Button primary loading={loading} onClick={handleClear} className="sm:mt-5 mt-6 w-full flex justify-center">
+        Clear
+      </Button>
+      <Button
+        primary
+        loading={loading}
+        disabled={disabled}
+        onClick={handleGenerate}
+        className="sm:mt-5 mt-6 w-full flex justify-center"
+      >
+        Generate
+      </Button>
+    </div>
+  )
+}
