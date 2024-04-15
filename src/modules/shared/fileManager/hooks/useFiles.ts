2c06f69b818224ea3123fcdfe22761129e060641
Jan Smrčka <jan.smrck@gmail.com>
2024-04-15 23:24:49 +0200

    some cleanup

--- src/modules/shared/fileManager/hooks/useFiles.ts
+++ src/modules/shared/fileManager/hooks/useFiles.ts
@@ -1,12 +1,38 @@
 import type { FileItem } from '../types'
 import axiosInstance from '@/core/axios/axiosInstance'
-import { useQuery } from 'react-query'
+import toast from 'react-hot-toast'
+import { useMutation, useQuery, useQueryClient } from 'react-query'
 
 const getFiles = (itemType: string, uid: string): Promise<Array<FileItem>> => {
   const endpoint = `/api/${itemType}/${uid}/files`
   return axiosInstance.get(endpoint).then(res => res.data)
 }
 
+const deleteFile = (
+  itemType: string,
+  uid: string,
+  id: string
+): Promise<string> => {
+  const endpoint = `/api/${itemType}/${uid}/files/${id}`
+  return axiosInstance.delete(endpoint).then(res => res.data)
+}
+
 export const useFiles = ({ itemType, uid }) => {
   return useQuery(['files', itemType, uid], () => getFiles(itemType, uid))
+}
+
+export const useFileDelete = ({ itemType, uid, id }) => {
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: () => deleteFile(itemType, uid, id),
+    onSuccess: () => {
+      queryClient.setQueryData<FileItem[]>(['files', itemType, uid], old => {
+        if (!old) return []
+        return old?.filter(obj => obj.id !== id)
+      })
+    },
+    onError: error => {
+      toast.error('Failed to delete file: ' + error)
+    }
+  })
 }
