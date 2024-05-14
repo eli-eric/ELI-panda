7f93082c98b23ae4971cbcb255c24ab6fb1d4641
JanSmrcka <jan.smrck@gmail.com>
2024-05-14 15:15:41 +0200

    fix graphql?

--- src/hooks/fetch/useGraphQL.ts
+++ src/hooks/fetch/useGraphQL.ts
@@ -1,5 +1,5 @@
-import request from 'graphql-request'
 import { type TypedDocumentNode } from '@graphql-typed-document-node/core'
+import { request } from 'graphql-request'
 import type {
   MutationFunction,
   UseMutationOptions,
