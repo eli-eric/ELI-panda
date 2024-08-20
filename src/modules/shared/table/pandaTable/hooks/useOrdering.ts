f0647a31577593d388f45647fa23d476a8e31e0d
Jan Smrčka <jan.smrck@gmail.com>
2024-08-20 12:10:37 +0200

    some small fixes

--- src/modules/shared/table/pandaTable/hooks/useOrdering.ts
+++ src/modules/shared/table/pandaTable/hooks/useOrdering.ts
@@ -30,7 +30,6 @@ export const useOrdering = (
           const updatedColumnOrder = columnOrder(storedOrder)
           setStoredOrder(enforceDefaultOrder(updatedColumnOrder))
         } else {
-          console.log('columnOrder', columnOrder)
           setStoredOrder(enforceDefaultOrder(columnOrder))
         }
       },
