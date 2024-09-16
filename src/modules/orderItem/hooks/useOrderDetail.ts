75a18c79e7c428daabac2f7a4684e0bdfceae164
JanSmrcka <jan.smrck@gmail.com>
2024-09-16 15:04:02 +0200

    fix refetch query in orders

--- src/modules/orderItem/hooks/useOrderDetail.ts
+++ src/modules/orderItem/hooks/useOrderDetail.ts
@@ -4,6 +4,7 @@ import { useSession } from 'next-auth/react'
 
 import { useEndpoint } from '@/hooks/fetch/useEndpoint'
 import { ROLE } from '@/types/constants/roles'
+import type { QueryFetcherKey } from '@/utils/fetcher'
 import { queryFetcher } from '@/utils/fetcher'
 
 import type { OrderDetailFormType } from '../types/form'
@@ -13,8 +14,10 @@ const useOrderDetail = () => {
   const uid = router.query.uid as string | undefined
   const { order: orderEndpoint } = useEndpoint({ uid })
 
-  const { data, isLoading, error, refetch } = useQuery({
-    queryKey: ['order', { uid }],
+  const queryKey: QueryFetcherKey = ['order', { uid }]
+
+  const { data, isLoading, error } = useQuery({
+    queryKey,
     queryFn: queryFetcher<OrderDetailFormType>('order'),
     enabled: !!uid,
     refetchOnMount: true
@@ -25,9 +28,9 @@ const useOrderDetail = () => {
 
   return {
     orderDetail: data,
-    invalidateQuery: refetch,
     loading: isLoading,
     error,
+    queryKey,
     disabledEdit,
     uid,
     orderEndpoint
