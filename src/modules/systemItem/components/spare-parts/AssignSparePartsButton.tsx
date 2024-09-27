a5220e0153d6787ecd7e08f6416cb65b2e096f0c
Jan Smrčka <jan.smrck@gmail.com>
2024-09-27 11:28:09 +0200

    add item usage filter for asign spare, add system code to table spare for

--- src/modules/systemItem/components/spare-parts/AssignSparePartsButton.tsx
+++ src/modules/systemItem/components/spare-parts/AssignSparePartsButton.tsx
@@ -48,6 +48,17 @@ export const AssignSparePartButton = () => {
                   name: parentTechnologicalUnit?.name,
                   uid: parentTechnologicalUnit?.uid
                 }
+              },
+              {
+                id: 'itemUsage',
+                name: 'itemUsage',
+                value: [
+                  '25c189d0-0564-43a7-90d9-65b7083bea98',
+                  '89d68bc5-82cc-45cf-80aa-8edb86bf52f1',
+                  '5defcd49-5307-4b21-94b1-870b8f61a919',
+                  '0c7a063d-2bb6-41ef-b808-a137e1deaaa0',
+                  'a5a2a316-fc23-45fd-b6b2-3dc2af4205ea'
+                ]
               }
             ])
             setSearch('for-system', systemDetail?.uid)
