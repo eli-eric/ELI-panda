d25b0f829d5acc5880b9cfcfc10f0b4dd7fc96a9
Jan Smrčka <jan.smrck@gmail.com>
2024-10-11 12:49:44 +0200

    fixing sharp nodemodules, update conversion of mini images

--- src/server/files/handler.ts
+++ src/server/files/handler.ts
@@ -19,7 +19,7 @@ const handler = (req: NextApiRequest, res: NextApiResponse) => {
     }
     switch (req.method) {
       case 'GET':
-        const { id } = getPathInfo(req, res)
+        const { id } = getPathInfo(req)
         return id ? downloadFile(req, res) : listFiles(req, res)
       case 'POST':
         return uploadFile(req, res)
