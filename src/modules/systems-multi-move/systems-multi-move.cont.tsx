ca2f006920331af1907d355a8d9f9c14f446ab61
Jan Smrčka <jan.smrck@gmail.com>
2024-11-26 14:12:57 +0100

    fix some issues for moving systems

--- src/modules/systems-multi-move/systems-multi-move.cont.tsx
+++ src/modules/systems-multi-move/systems-multi-move.cont.tsx
@@ -1,3 +1,5 @@
+import { useCallback } from 'react'
+
 import type { SystemDetail } from '@/types/responses/systems'
 import { classNames } from '@/utils'
 
@@ -12,38 +14,46 @@ export const SystemsMultiMoveContainer = () => {
     movingSystemsTableId
   } = useSystemsMoveStore()
 
-  const canSelectMovingSystem = (system: SystemDetail) => {
-    const isSelectedSameDestination = destinationSystem?.uid === system.uid
+  console.log('render movingSystems', movingSystems)
 
-    const isSelectedParent = system.parentPath?.some(path =>
-      movingSystems.some(moving => moving.uid === path.uid)
-    )
+  const canSelectMovingSystem = useCallback(
+    (system: SystemDetail) => {
+      const isSelectedSameDestination = destinationSystem?.uid === system.uid
 
-    if (isSelectedSameDestination || isSelectedParent) {
-      return false
-    }
-    return true
-  }
+      const isSelectedParent = system.parentPath?.some(path =>
+        movingSystems.some(moving => moving.uid === path.uid)
+      )
 
-  const canSelectDestinationSystem = (system: SystemDetail) => {
-    const isSelectedSameMoving = movingSystems.some(
-      movingSystem => movingSystem.uid === system.uid
-    )
+      if (isSelectedSameDestination || isSelectedParent) {
+        return false
+      }
+      return true
+    },
+    [movingSystems, destinationSystem]
+  )
 
-    const isSelectedParent = system.parentPath?.some(path =>
-      movingSystems.some(moving => moving.uid === path.uid)
-    )
+  const canSelectDestinationSystem = useCallback(
+    (system: SystemDetail) => {
+      const isSelectedSameMoving = movingSystems.some(
+        movingSystem => movingSystem.uid === system.uid
+      )
 
-    if (
-      movingSystems.length === 0 ||
-      isSelectedSameMoving ||
-      isSelectedParent
-    ) {
-      return false
-    }
+      const isSelectedParent = system.parentPath?.some(path =>
+        movingSystems.some(moving => moving.uid === path.uid)
+      )
 
-    return destinationSystem ? destinationSystem?.uid === system.uid : true
-  }
+      if (
+        movingSystems.length === 0 ||
+        isSelectedSameMoving ||
+        isSelectedParent
+      ) {
+        return false
+      }
+
+      return destinationSystem ? destinationSystem?.uid === system.uid : true
+    },
+    [movingSystems, destinationSystem]
+  )
 
   return (
     <div className={classNames('grid grid-cols-2')}>
