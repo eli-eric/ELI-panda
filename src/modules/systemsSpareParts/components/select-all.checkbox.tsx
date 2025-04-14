735009eabdc8d6ba7e34dae750f15e5c3f470241
Jan Smrcka <jan.smrck@gmail.com>
2025-04-14 10:15:59 +0200

    ELIPANDA-388

---
+++ src/modules/systemsSpareParts/components/select-all.checkbox.tsx
@@ -0,0 +1,64 @@
+import type { Table } from '@tanstack/react-table'
+import type { HTMLProps } from 'react'
+import { useState } from 'react'
+
+import type { SystemDetail } from '@/types/responses/systems'
+import { cx } from '@/utils'
+
+interface IndeterminateCheckboxProps extends HTMLProps<HTMLInputElement> {
+  table: Table<SystemDetail>
+}
+
+export function SelectAllCheckbox({
+  table,
+  setSelectedUids,
+  ...rest
+}: IndeterminateCheckboxProps) {
+  const [checked, setChecked] = useState(false)
+
+  const handleToggleSelectAllTopLevel = () => {
+    const topLevelRowIds = table
+      .getRowModel()
+      .rows.map(row => (row.id.includes('.') ? row.id.split('.')[0] : row.id))
+      .filter((value, index, self) => self.indexOf(value) === index)
+
+    table.setRowSelection(() => {
+      const newSelection = {}
+      // Select all top-level rows
+      topLevelRowIds.forEach(id => {
+        newSelection[id] = true
+      })
+      return newSelection
+    })
+
+    const topLevelSystemsUids = topLevelRowIds.map(
+      id => table.getRow(id).original.uid
+    )
+    setSelectedUids(topLevelSystemsUids)
+  }
+
+  const onChange = e => {
+    if (e.target.checked) {
+      setChecked(e.target.checked)
+      handleToggleSelectAllTopLevel()
+    } else {
+      setChecked(e.target.checked)
+      setSelectedUids([])
+      table.setRowSelection({})
+    }
+  }
+
+  return (
+    <input
+      type="checkbox"
+      className={cx(
+        'cursor-pointer',
+        'focus:ring-primary-500 h-5 w-5 text-primary-600 dark:text-primary-600 rounded',
+        !checked && 'dark:bg-gray-700'
+      )}
+      onChange={onChange}
+      checked={checked}
+      {...rest}
+    />
+  )
+}
