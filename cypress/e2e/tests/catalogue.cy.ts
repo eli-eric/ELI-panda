300c98c75c4ae67fa3274b2d83640e984b5ab0d8
Jan Smrcka <jan.smrck@gmail.com>
2025-07-11 10:56:39 +0200

    remove cypress, will be replaced with playwright

--- cypress/e2e/tests/catalogue.cy.ts
+++
@@ -1,9 +0,0 @@
-import { testCredAdmin } from 'cypress/support/testData'
-import { aliasQuery } from '../graphql-test-utils'
-
-describe('Catalogue', () => {
-  beforeEach(() => {
-    cy.visit(Cypress.env('host'))
-    cy.loginNextAuth(testCredAdmin)
-  })
-})
