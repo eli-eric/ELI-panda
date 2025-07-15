d30fc5417477cc821c67e44c4898742053aad3f6
Jan Smrcka <jan.smrck@gmail.com>
2025-07-15 20:54:46 +0200

    fix tooltip withshadcn and radix

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -1,6 +1,20 @@
-import type { TippyProps } from '@tippyjs/react'
-import Tippy from '@tippyjs/react'
-
-export const Tooltip = ({ children, ...rest }: TippyProps) => (
-  <Tippy {...rest}>{children}</Tippy>
-)
+type Props = {
+  content?: string
+  children: React.ReactNode
+}
+import {
+  Tooltip as TP,
+  TooltipContent,
+  TooltipTrigger
+} from '@/components/ui/tooltip'
+export const Tooltip = ({ children, content }: Props) =>
+  content ? (
+    <TP>
+      <TooltipTrigger asChild>{children}</TooltipTrigger>
+      <TooltipContent>
+        <p>{content}</p>
+      </TooltipContent>
+    </TP>
+  ) : (
+    children
+  )
