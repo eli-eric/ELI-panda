80ac018d6a3d8e038d7fd261db50a00ed77ba071
Jan Smrcka <jan.smrck@gmail.com>
2025-07-16 08:50:11 +0200

    fix service detail layout

--- src/modules/systemItem/utils/index.ts
+++ src/modules/systemItem/utils/index.ts
@@ -4,8 +4,8 @@ export const getColorBySystemLevel = (level?: SystemLevel, index?: number) => {
   switch (level) {
     case SystemLevel.KeySystems:
       return index && index % 2 === 0
-        ? 'bg-primary-50 dark:bg-primary-300'
-        : 'bg-primary-100 dark:bg-primary-400'
+        ? 'bg-orange-50 dark:bg-orange-400/70'
+        : 'bg-orange-100 dark:bg-orange-600/70'
     case SystemLevel.SubsystemsAndParts:
       return index && index % 2 === 0
         ? 'bg-sky-50 dark:bg-sky-600 dark:text-gray-200'
