76661b0948fd3c30112cb824e4247dab6ee4258c
Vojtech Mrkos <vojtech.mrkos@eli-beams.eu>
2025-09-19 08:10:17 +0200

    added dirty protection

--- src/modules/catalogue/components/categoryEdit/form/CategoryEdit.form.tsx
+++ src/modules/catalogue/components/categoryEdit/form/CategoryEdit.form.tsx
@@ -1,9 +1,12 @@
 import { yupResolver } from '@hookform/resolvers/yup'
+import { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 
 import { Form } from '@/components/form/Form'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { useFormDirtyProtection } from '@/hooks/useFormDirtyProtection'
 import type { ImageGalleryRef } from '@/modules/shared/imageManager/types'
+import { useModalFormStateStore } from '@/store/useModalFormStateStore'
 import type { CodebookType } from '@/types/responses/codebook'
 
 import type { CategoryFormType } from '../types'
@@ -15,7 +18,9 @@ import { PhysicalItemProperties } from './components/PhysicalItemProperties'
 interface Props {
   uid?: string
   onSubmit: (data: CategoryFormType) => void
-  children: React.ReactNode
+  children: (
+    withDirtyProtection: (callback: () => void) => () => void
+  ) => React.ReactNode
   systemType?: CodebookType
   categoryDetail: CategoryFormType
   imageRef?: React.MutableRefObject<ImageGalleryRef | null>
@@ -35,14 +40,28 @@ const CategoryEditForm = ({
           systemType
         }
       : categoryDetail,
-    resolver: yupResolver(categoryValidationschema)
+    resolver: yupResolver(categoryValidationschema),
+    mode: 'onChange'
   })
+
+  const { withDirtyProtection } = useFormDirtyProtection(formMethods)
+  const { setIsDirty, reset } = useModalFormStateStore()
+
+  useEffect(() => {
+    setIsDirty(formMethods.formState.isDirty)
+  }, [formMethods.formState.isDirty, setIsDirty])
+
+  useEffect(() => {
+    return () => {
+      reset()
+    }
+  }, [reset])
 
   return (
     <Form formMethods={formMethods} onSubmit={onSubmit}>
       <div className="space-y-6">
         {/* Action Buttons */}
-        {children}
+        {children(withDirtyProtection)}
 
         {/* Basic Information */}
         <Card>
