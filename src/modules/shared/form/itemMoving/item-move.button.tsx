1f9c475780058b83172228f41061d1bcad4628e4
Jan Smrcka <jan.smrck@gmail.com>
2025-09-23 13:45:13 +0200

    refactor: simplify item move button implementation and modal handling

--- src/modules/shared/form/itemMoving/item-move.button.tsx
+++ src/modules/shared/form/itemMoving/item-move.button.tsx
@@ -1,28 +1,9 @@
-import dynamic from 'next/dynamic'
 import type { FC } from 'react'
-import { Fragment, Suspense } from 'react'
 
 import { Button } from '@/components/Buttons'
 
-import { useModalWizardStore } from './store/useModalWizardStore'
-
-const ItemMoveModal = dynamic(() =>
-  import('./item-move.modal').then(mod => mod.ItemMoveModal)
-)
+import { openItemMoveModal } from './item-move.modal'
 
 export const ItemMoveButton: FC = () => {
-  const { setOpen } = useModalWizardStore()
-
-  function handleShow() {
-    setOpen(true)
-  }
-
-  return (
-    <Fragment>
-      <Button onClick={handleShow}>Move Item</Button>
-      <Suspense>
-        <ItemMoveModal />
-      </Suspense>
-    </Fragment>
-  )
+  return <Button onClick={openItemMoveModal}>Move Item</Button>
 }
